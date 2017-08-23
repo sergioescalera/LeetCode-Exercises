941de17da357df7cf49c2510bb711023cbec34b0
Sergio <sergio.escalera.c@gmail.com>
2017-08-23 17:44:47 -0400

    Solved Two Sum exercise with a dictionary.

--- src/LeetCode-Exercises/Exercises/001-TwoSum.cs
+++ src/LeetCode-Exercises/Exercises/001-TwoSum.cs
@@ -1,5 +1,6 @@
 ﻿using LeetCode.Attributes;
 using System;
+using System.Collections.Generic;
 
 namespace LeetCode.Exercises
 {
@@ -8,15 +9,16 @@ namespace LeetCode.Exercises
     {
         public static void Run()
         {
-            var numbers = new[] { 2, 7, 11, 15 };
-            var target = 22;
+            var numbers = new[] { 3, 2, 4/*2, 7, 11, 11, 15*/ };
+            var target = 6;//22;
 
-            var solution = FindPair(numbers, target);
+            //var solution = FindBruteForce(numbers, target);
+            var solution = FindDictionary(numbers, target);
 
             Console.WriteLine($"Two Sum Result: {String.Join(", ", solution)}");
         }
 
-        private static Int32[] FindPair(Int32[] nums, Int32 target)
+        private static Int32[] FindBruteForce(Int32[] nums, Int32 target)
         {
             for (int i = 0; i < nums.Length - 1; i++)
             {
@@ -25,6 +27,25 @@ namespace LeetCode.Exercises
                     if (nums[i] + nums[j] == target)
                         return new[] { i, j };
                 }
+            }
+
+            return new int[0];
+        }
+
+        private static Int32[] FindDictionary(Int32[] nums, Int32 target)
+        {
+            var dictionary = new Dictionary<Int32, Int32>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                var current = nums[i];
+                var diff = target - current;
+
+                if (dictionary.ContainsKey(diff))
+                    return new[] { dictionary[diff], i };
+
+                if (!dictionary.ContainsKey(current))
+                    dictionary.Add(current, i);
             }
 
             return new int[0];
