809ee4af05acef68a46a73abed594f25d4e528ca
Sergio Escalera <sergio.escalera.c@gmail.com>
2020-02-19 20:16:45 -0500

    Removed & sorted usings

--- src/LeetCode-Exercises/Exercises/026-RemoveDuplicatesSortedArray.cs
+++ src/LeetCode-Exercises/Exercises/026-RemoveDuplicatesSortedArray.cs
@@ -1,9 +1,6 @@
 ﻿using LeetCode.Attributes;
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace LeetCode.Exercises
 {
