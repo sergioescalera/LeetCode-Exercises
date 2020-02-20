815eb7e30d7cfda6d55468551d3612616a3a8584
Sergio Escalera <sergio.escalera.c@gmail.com>
2020-02-20 12:03:37 -0500

    Added remove elem solution

---
+++ src/LeetCode-Exercises/Exercises/027-RemoveElement.cs
@@ -0,0 +1,49 @@
+﻿using LeetCode.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Exercises
+{
+    [Exercise]
+    public class RemoveElementExercise
+    {
+        public static void Run()
+        {
+            var exercise = new RemoveElementExercise();
+
+            var solution = exercise.RemoveElement(
+                new[] { 0, 1, 2, 2, 3, 0, 4, 2 },
+                2);
+
+
+        }
+
+        public int RemoveElement(int[] nums, int val)
+        {
+            if (nums == null)
+                return 0;
+
+            if (nums.Length == 0)
+                return 0;
+
+            var count = 0;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] == val)
+                {
+                    continue;
+                }
+
+                nums[count] = nums[i];
+
+                count++;
+            }
+
+            return count;
+        }
+    }
+}
