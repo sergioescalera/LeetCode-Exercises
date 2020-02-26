061986a6262843c622d7a74bea963aff44d48ba7
Sergio Escalera <sergio.escalera.c@gmail.com>
2020-02-26 11:23:40 -0500

    Added search insert position

---
+++ src/LeetCode-Exercises/Exercises/035-SearchInsertPosition.cs
@@ -0,0 +1,76 @@
+﻿using LeetCode.Attributes;
+using LeetCode.Extensions;
+using System;
+
+namespace LeetCode.Exercises
+{
+    [Exercise]
+    public class SearchInsertPosition
+    {
+        public static void Run()
+        {
+            var exercise = new SearchInsertPosition();
+
+            do
+            {
+                var nums = exercise.ReadNums();
+                var target = exercise.ReadNum();
+
+                if (nums == null || target == null)
+                {
+                    return;
+                }
+
+                var solution = exercise.SearchInsert(nums, target.Value, 0, nums.Length - 1);
+
+                Console.WriteLine("Position is {0}", solution);
+                Console.WriteLine();
+
+            } while (true);
+        }
+
+        public int SearchInsert(int[] nums, int target)
+        {
+            if (nums == null)
+            {
+                return -1;
+            }
+
+            if (nums.Length == 0)
+            {
+                return 0;
+            }
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                var n = nums[i];
+
+                if (n >= target)
+                {
+                    return i;
+                }
+            }
+
+            return nums.Length;
+        }
+
+        public int SearchInsert(int[] nums, int target, int start, int end)
+        {
+            if (start >= end)
+            {
+                return nums[start] >= target ? start : start + 1;
+            }
+
+            var middle = (start + end) / 2 + 1;
+
+            var n = nums[middle];
+
+            if (n >= target)
+            {
+                return SearchInsert(nums, target, start, middle - 1);
+            }
+
+            return SearchInsert(nums, target, middle, end);
+        }
+    }
+}
---
+++ src/LeetCode-Exercises/Extensions/ExerciseExtensions.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+
+namespace LeetCode.Extensions
+{
+    public static class ExerciseExtensions
+    {
+        public static Int32? ReadNum(this Object obj)
+        {
+            Console.WriteLine("Enter number...");
+
+            var line = Console.ReadLine();
+
+            if (Int32.TryParse(line, out var n))
+            {
+                return n;
+            }
+
+            return null;
+        }
+
+        public static Int32[] ReadNums(this Object obj)
+        {
+            Console.WriteLine("Enter numbers...");
+
+            var line = Console.ReadLine();
+
+            if (String.IsNullOrEmpty(line))
+            {
+                return null;
+            }
+
+            var array = line.Split(',', ';', ' ');
+
+            return array
+                .Where(o => String.IsNullOrWhiteSpace(o) == false)
+                .Select(o => Int32.TryParse(o, out var n) ? n : default(Int32?))
+                .Where(o => o.HasValue)
+                .Select(o => o.GetValueOrDefault())
+                .ToArray();
+        }
+    }
+}
