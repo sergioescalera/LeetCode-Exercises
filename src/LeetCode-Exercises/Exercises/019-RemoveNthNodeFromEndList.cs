138fc9253a843863a0ccb62ae412a2772536bb44
Sergio Escalera <sergio.escalera.c@gmail.com>
2020-03-30 23:11:49 -0400

    Added Remove nth from end of list

---
+++ src/LeetCode-Exercises/Exercises/019-RemoveNthNodeFromEndList.cs
@@ -0,0 +1,45 @@
+﻿using LeetCode.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Exercises
+{
+    public class RemoveNthNodeFromEndList
+    {
+        public ListNode RemoveNthFromEnd(ListNode head, int n)
+        {
+            if (n <= 0)
+                return head;
+
+            return RemoveNthFromEnd(head, n, out var i);
+        }
+
+        private ListNode RemoveNthFromEnd(ListNode current, int n, out int i)
+        {
+            if (current == null)
+            {
+                i = 0;
+
+                return current;
+            }
+
+            var next = RemoveNthFromEnd(current.next, n, out var j);
+
+            i = j + 1;
+
+            if (i == n)
+            {
+                return next;
+            }
+            else
+            {
+                current.next = next;
+
+                return current;
+            }
+        }
+    }
+}
