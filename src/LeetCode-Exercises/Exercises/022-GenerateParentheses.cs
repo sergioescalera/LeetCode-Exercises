ef6550accd553b7f0885b1de13a1b891240360a4
Sergio Escalera <sergio.escalera.c@gmail.com>
2020-04-07 20:24:25 -0400

    Added generate parentheses solution

---
+++ src/LeetCode-Exercises/Exercises/022-GenerateParentheses.cs
@@ -0,0 +1,82 @@
+﻿using LeetCode.Attributes;
+using LeetCode.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Exercises
+{
+    [Exercise]
+    public class GenerateParenthesesExercise
+    {
+        public static void Run()
+        {
+            var exercise = new GenerateParenthesesExercise();
+
+            do
+            {
+                var n = exercise.ReadNum();
+
+                if (n == null) break;
+
+                var s = exercise.GenerateParenthesis(n.Value);
+
+                foreach (var item in s)
+                {
+                    Console.WriteLine(item);
+                }
+
+            } while (true);
+        }
+
+        public IList<string> GenerateParenthesis(int n)
+        {
+            if (n <= 0)
+            {
+                return new string[] { "" };
+            }
+
+            var list = new List<string>();
+
+            var sucess = GenerateParenthesis(n, n, n, "", list);
+
+            return list;
+        }
+
+        private bool GenerateParenthesis(
+            int n, 
+            int open, 
+            int close, 
+            string current,
+            List<string> list)
+        {
+            if (open == 0 && close == 0)
+            {
+                return true;
+            }
+
+            if (open > close)
+            {
+                return false;
+            }
+
+            if (open == 0)
+            {
+                list.Add(current + new string(')', close));
+
+                return true;
+            }
+
+            GenerateParenthesis(n, open - 1, close, current + "(", list);
+
+            if (close > open)
+            {
+                GenerateParenthesis(n, open, close - 1, current + ")", list);
+            }
+
+            return list.Count > 0;
+        }
+    }
+}
