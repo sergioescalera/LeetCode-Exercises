4c2e01bee760861e6189e21b4ae6295983bcb892
Sergio Escalera <sergio.escalera.c@gmail.com>
2020-05-01 11:27:11 -0400

    Added swap nodes in pairs solution

---
+++ src/LeetCode-Exercises/Exercises/024-SwapNodesInPairs.cs
@@ -0,0 +1,30 @@
+﻿using LeetCode.Attributes;
+using LeetCode.Input;
+
+namespace LeetCode.Exercises
+{
+    [Exercise]
+    public class SwapNodesInPairs
+    {
+        public static void Run()
+        {
+
+        }
+
+        public ListNode SwapPairs(ListNode head)
+        {
+            if (head == null || head.next == null)
+            {
+                return head;
+            }
+
+            var next = head.next;
+
+            head.next = SwapPairs(next.next);
+
+            next.next = head;
+
+            return next;
+        }
+    }
+}
