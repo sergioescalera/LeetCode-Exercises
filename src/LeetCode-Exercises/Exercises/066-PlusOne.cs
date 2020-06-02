2d7c9d9ee3a06e7d2c3b2d8c29ce0c0eb3007eb3
Sergio Escalera <sergio.escalera.c@gmail.com>
2020-06-02 19:44:59 -0400

    Plus one exercise

---
+++ src/LeetCode-Exercises/Exercises/066-PlusOne.cs
@@ -0,0 +1,46 @@
+﻿using LeetCode.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Exercises
+{
+    [Exercise]
+    public class PlusOneExercise
+    {
+        public static void Run()
+        {
+
+        }
+
+        public int[] PlusOne(int[] digits)
+        {
+            if (digits == null)
+            {
+                return digits;
+            }
+
+            for (int i = digits.Length - 1; i >= 0; i--)
+            {
+                var d = digits[i];
+
+                if (d < 9)
+                {
+                    digits[i] = d + 1;
+
+                    return digits;
+                }
+
+                digits[i] = 0;
+            }
+
+            var array = new int[digits.Length + 1];
+
+            array[0] = 1;
+
+            return array;
+        }
+    }
+}
