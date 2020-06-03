03632710ea4ddcb9b8c6cfdced3a6f56a71f3d41
Sergio Escalera <sergio.escalera.c@gmail.com>
2020-06-02 20:06:05 -0400

    Remove sorded list duplicates

---
+++ src/LeetCode-Exercises/Exercises/083-RemoveSortedListDuplicates.cs
@@ -0,0 +1,34 @@
+﻿using LeetCode.Attributes;
+using LeetCode.Input;
+
+namespace LeetCode.Exercises
+{
+    [Exercise]
+    public class RemoveSortedListDuplicates
+    {
+        public ListNode DeleteDuplicates(ListNode head)
+        {
+            if (head == null)
+                return null;
+
+            head.next = DeleteDuplicates(head.next, head.val);
+
+            return head;
+        }
+
+        private ListNode DeleteDuplicates(ListNode current, int previous)
+        {
+            if (current == null)
+                return null;
+
+            if (current.val == previous)
+            {
+                return DeleteDuplicates(current.next, previous);
+            }
+
+            current.next = DeleteDuplicates(current.next, current.val);
+
+            return current;
+        }
+    }
+}
