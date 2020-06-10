690e389ea5ce932025ef83ba6ae1ac3a4782d1ac
Sergio Escalera <sergio.escalera.c@gmail.com>
2020-06-10 17:18:49 -0400

    Group Anagrams using linq :)

---
+++ src/LeetCode-Exercises/Exercises/049-GroupAnagrams.cs
@@ -0,0 +1,36 @@
+﻿using LeetCode.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode.Exercises
+{
+    [Exercise]
+    public class GroupAnagramsExercise
+    {
+        public static void Run()
+        {
+            var exercise = new GroupAnagramsExercise();
+
+            var input = new[] { "eat", "tea", "tan", "ate", "nat", "bat" };
+
+            var result = exercise.GroupAnagrams(input);
+        }
+
+        public IList<IList<string>> GroupAnagrams(string[] strs)
+        {
+            if (strs == null)
+                throw new ArgumentNullException(nameof(strs));
+
+            if (strs.Length == 0)
+                return new string[0][];
+
+            var groups = strs
+                .GroupBy(str => String.Concat(str.OrderBy(c => c)))
+                .Select(o => o.ToArray())
+                .ToArray();
+
+            return groups;
+        }
+    }
+}
