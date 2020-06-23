f4d4d601adfc210264c473eab764be4b1c52d778
Sergio Escalera <sergio.escalera.c@gmail.com>
2020-06-23 15:52:56 -0400

    Simplify path

---
+++ src/LeetCode-Exercises/Exercises/071-SimplifyPath.cs
@@ -0,0 +1,69 @@
+﻿using LeetCode.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode.Exercises
+{
+    [Exercise]
+    public class SimplifyPathExercise
+    {
+        public static void Run()
+        {
+            var exercise = new SimplifyPathExercise();
+
+            do
+            {
+                Console.WriteLine("Enter path");
+
+                var line = Console.ReadLine();
+
+                if (string.IsNullOrWhiteSpace(line))
+                {
+                    break;
+                }
+
+                var path = exercise.SimplifyPath(line);
+
+                Console.WriteLine(path);
+
+                Console.WriteLine();
+
+            } while (true);
+        }
+
+        public string SimplifyPath(string path)
+        {
+            if (String.IsNullOrWhiteSpace(path))
+                return path;
+
+            var items = path.Split(new[] { '/' }, StringSplitOptions.RemoveEmptyEntries);
+
+            var stack = new Stack<string>();
+
+            foreach (var item in items)
+            {
+                if (item == ".")
+                {
+                    continue;
+                }
+
+                if (item == ".." && stack.Count == 0)
+                {
+                    continue;
+                }
+
+                if (item == "..")
+                {
+                    stack.Pop();
+                }
+                else
+                {
+                    stack.Push(item);
+                }
+            }
+
+            return "/" + String.Join("/", stack.Reverse());
+        }
+    }
+}
