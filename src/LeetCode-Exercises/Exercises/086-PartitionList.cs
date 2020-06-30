290200d1575c653f6eb4ea320d9b56dab5aa4f4d
Sergio Escalera <sergio.escalera.c@gmail.com>
2020-06-30 18:22:35 -0400

    Partition list

---
+++ src/LeetCode-Exercises/Exercises/086-PartitionList.cs
@@ -0,0 +1,63 @@
+﻿using LeetCode.Attributes;
+using LeetCode.Input;
+
+namespace LeetCode.Exercises
+{
+    [Exercise]
+    public class PartitionList
+    {
+        public ListNode Partition(ListNode head, int x)
+        {
+            if (head == null)
+                return null;
+
+            ListNode headEnd = null;
+            ListNode tail = null;
+            ListNode tailEnd = null;
+            ListNode current = head;
+
+            head = null;
+
+            while (current != null)
+            {
+                var next = current.next;
+
+                current.next = null;
+
+                if (current.val < x)
+                {
+                    if (head == null)
+                    {
+                        head = current;
+                    }
+                    if (headEnd != null)
+                    {
+                        headEnd.next = current;
+                    }
+                    headEnd = current;
+                }
+                else
+                {
+                    if (tail == null)
+                    {
+                        tail = current;
+                    }
+                    if (tailEnd != null)
+                    {
+                        tailEnd.next = current;
+                    }
+                    tailEnd = current;
+                }
+
+                current = next;
+            }
+
+            if (headEnd != null && headEnd != tail)
+            {
+                headEnd.next = tail;
+            }
+
+            return head ?? tail;
+        }
+    }
+}
