b912704b71326ba4fadff5eb7ea550ed6a7f783a
Sergio Escalera <sergio.escalera.c@gmail.com>
2020-07-17 15:54:16 -0400

    Is Subtree

---
+++ src/LeetCode-Exercises/Exercises/572-SubtreeAnotherTree.cs
@@ -0,0 +1,46 @@
+﻿using LeetCode.Attributes;
+using LeetCode.Input;
+
+namespace LeetCode.Exercises
+{
+    [Exercise]
+    public class SubtreeAnotherTree
+    {
+        public static void Run()
+        {
+        }
+
+        public bool IsSubtree(TreeNode s, TreeNode t)
+        {
+            if (t == null)
+                return s == null;
+
+            if (s == null)
+                return false;
+
+            if (s.val == t.val &&
+                IsSame(s.left, t.left) &&
+                IsSame(s.right, t.right))
+            {
+                return true;
+            }
+
+            return IsSubtree(s.left, t) || IsSubtree(s.right, t);
+        }
+
+        private bool IsSame(TreeNode a, TreeNode b)
+        {
+            if (a == null)
+                return b == null;
+
+            if (b == null)
+                return false;
+
+            if (a.val != b.val)
+                return false;
+
+            return IsSame(a.left, b.left)
+                && IsSame(a.right, b.right);
+        }
+    }
+}
