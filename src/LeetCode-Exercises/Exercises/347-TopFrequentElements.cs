da9e4a78f9fb2f678b6f1ec2d757c81d6f1b95ea
Sergio Escalera <sergio.escalera.c@gmail.com>
2020-07-17 12:58:28 -0400

    Removed unused using.

--- src/LeetCode-Exercises/Exercises/347-TopFrequentElements.cs
+++ src/LeetCode-Exercises/Exercises/347-TopFrequentElements.cs
@@ -1,10 +1,7 @@
 ﻿using LeetCode.Attributes;
 using System;
-using System.CodeDom;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace LeetCode.Exercises
 {
