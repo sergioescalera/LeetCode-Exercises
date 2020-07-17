57af9e9783929bb860a9d81220dc266740b16964
Sergio Escalera <sergio.escalera.c@gmail.com>
2020-07-17 12:41:15 -0400

    Top K frequent elems

---
+++ src/LeetCode-Exercises/Exercises/347-TopFrequentElements.cs
@@ -0,0 +1,71 @@
+﻿using LeetCode.Attributes;
+using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Exercises
+{
+    [Exercise]
+    public class TopFrequentElements
+    {
+        public static void Run()
+        {
+
+        }
+
+        public int[] TopKFrequent(int[] nums, int k)
+        {
+            if (nums == null)
+                return null;
+
+            if (nums.Length == 0)
+                return nums;
+
+            if (k <= 0)
+                return new int[0];
+
+            var values = (int[])nums.Clone();
+
+            Array.Sort(values);
+
+            var list = new List<Item>();
+            var last = default(Item);
+
+            for (int i = 0; i < values.Length; i++)
+            {
+                var n = values[i];
+
+                if (last == null || last.val != n)
+                {
+                    last = new Item
+                    {
+                        val = n,
+                        count = 1
+                    };
+
+                    list.Add(last);
+                }
+                else
+                {
+                    last.count++;
+                }
+            }
+
+            return list
+                .OrderByDescending(o => o.count)
+                .Take(k)
+                .Select(o => o.val)
+                .ToArray();
+        }
+
+        class Item
+        {
+            public int val;
+
+            public int count;
+        }
+    }
+}
