566abd68472d61131d5981314a2278a20b2b9f11
Sergio Escalera <sergio.escalera.c@gmail.com>
2020-07-23 15:14:51 -0400

    Single num III using hash set

---
+++ src/LeetCode-Exercises/Exercises/260-SingleNumber.cs
@@ -0,0 +1,41 @@
+﻿using LeetCode.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode.Exercises
+{
+    [Exercise]
+    public class SingleNumberIII
+    {
+        public int[] SingleNumber(int[] nums)
+        {
+            if (nums == null)
+                throw new ArgumentNullException(nameof(nums));
+
+            if (nums.Length == 0)
+                throw new ArgumentException();
+
+            if (nums.Length <= 2)
+                return nums;
+
+            var set = new HashSet<int>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                var n = nums[i];
+
+                if (set.Contains(n))
+                {
+                    set.Remove(n);
+                }
+                else
+                {
+                    set.Add(n);
+                }
+            }
+
+            return set.ToArray();
+        }
+    }
+}
