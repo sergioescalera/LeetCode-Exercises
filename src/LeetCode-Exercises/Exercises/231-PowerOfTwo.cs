0313b20d085f4b73c0a87aa4fbfb975f61daf625
Sergio Escalera <sergio.escalera.c@gmail.com>
2020-08-04 12:48:49 -0400

    Power of 2, Power of 4

---
+++ src/LeetCode-Exercises/Exercises/231-PowerOfTwo.cs
@@ -0,0 +1,38 @@
+﻿using LeetCode.Attributes;
+using LeetCode.Extensions;
+using System;
+
+namespace LeetCode.Exercises
+{
+    [Exercise]
+    public class PowerOfTwo
+    {
+        public static void Run()
+        {
+            var ex = new PowerOfTwo();
+
+            do
+            {
+                var n = ex.ReadNum();
+
+                if (n == null)
+                    break;
+
+                var isPower = ex.IsPowerOfTwo(n.Value);
+
+                Console.WriteLine(isPower);
+
+            } while (true);
+        }
+
+        public bool IsPowerOfTwo(int n)
+        {
+            if (n < 1)
+            {
+                return false;
+            }
+
+            return (n & (n - 1)) == 0;
+        }
+    }
+}
---
+++ src/LeetCode-Exercises/Exercises/342-PowerOfFour.cs
@@ -0,0 +1,43 @@
+﻿using LeetCode.Attributes;
+using LeetCode.Extensions;
+using System;
+using System.Globalization;
+
+namespace LeetCode.Exercises
+{
+    [Exercise]
+    public class PowerOfFour
+    {
+        public static void Run()
+        {
+            var ex = new PowerOfFour();
+
+            do
+            {
+                var n = ex.ReadNum();
+
+                if (n == null)
+                    break;
+
+                var isPower = ex.IsPowerOfFour(n.Value);
+
+                Console.WriteLine(isPower);
+
+            } while (true);
+        }
+
+        public bool IsPowerOfFour(int num)
+        {
+            if (num < 1)
+            {
+                return false;
+            }
+
+            var log = Math.Log(num, 4);
+
+            var ceil = Math.Ceiling(log);
+
+            return ceil == num;
+        }
+    }
+}
