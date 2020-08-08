d5a438b508d0d530536a49b0ea892723ed996df7
Sergio Escalera <sergio.escalera.c@gmail.com>
2020-08-08 12:41:35 -0400

    Path sum II

---
+++ src/LeetCode-Exercises/Exercises/113-PathSumII.cs
@@ -0,0 +1,49 @@
+﻿using LeetCode.Attributes;
+using LeetCode.Input;
+using System.Collections.Generic;
+
+namespace LeetCode.Exercises
+{
+    [Exercise]
+    public class PathSumII
+    {
+        public IList<IList<int>> PathSum(TreeNode root, int sum)
+        {
+
+            if (root == null)
+            {
+                return new List<IList<int>> { };
+            }
+
+            if (root.val == sum && root.left == null && root.right == null)
+            {
+                return new List<IList<int>> { new List<int>() { root.val } };
+            }
+
+            var val = sum - root.val;
+
+            var paths = new List<IList<int>>();
+
+            AddPaths(paths, PathSum(root.left, val), root.val);
+
+            AddPaths(paths, PathSum(root.right, val), root.val);
+
+            return paths;
+        }
+
+        private void AddPaths(List<IList<int>> list, IList<IList<int>> paths, int val)
+        {
+            if (paths == null)
+            {
+                return;
+            }
+
+            foreach (var path in paths)
+            {
+                path.Insert(0, val);
+
+                list.Add(path);
+            }
+        }
+    }
+}
