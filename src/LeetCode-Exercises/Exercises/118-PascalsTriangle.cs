e504a915cbf6dc3ddb8696345d72aa81d912c42d
Sergio Escalera <sergio.escalera.c@gmail.com>
2020-08-12 21:03:05 -0400

    118, 119 Pascal's Triangle

---
+++ src/LeetCode-Exercises/Exercises/118-PascalsTriangle.cs
@@ -0,0 +1,83 @@
+﻿using LeetCode.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode.Attributes
+{
+    [Exercise]
+    public class PascalsTriangleII
+    {
+        public static void Run()
+        {
+            var ex = new PascalsTriangleII();
+
+            do
+            {
+                var n = ex.ReadNum("Enter row index");
+
+                if (n == null)
+                {
+                    break;
+                }
+
+                var list = ex.GetRow(n.Value);
+                
+                foreach (var row in list)
+                {
+                    Console.WriteLine(String.Join(", ", row));
+                }
+                Console.WriteLine();
+
+            } while (true);
+        }
+
+        static IDictionary<int, IList<int>> cache = new Dictionary<int, IList<int>>();
+
+        public IList<IList<int>> GetRow(int rowIndex)
+        {
+            if (rowIndex < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(rowIndex));
+            }
+
+            if (rowIndex == 0)
+            {
+                return new List<IList<int>> { new[] { 1 } };
+            }
+
+            if (cache.ContainsKey(rowIndex))
+            {
+                return cache.Values.ToList();
+            }
+
+            var list = GetRow(rowIndex - 1);
+
+            var previousRow = list.Last();
+
+            var row = new int[previousRow.Count + 1];
+
+            for (int i = 0; i < row.Length; i++)
+            {
+                if (i == 0)
+                {
+                    row[0] = previousRow[0];
+                }
+                else if (i == row.Length - 1)
+                {
+                    row[row.Length - 1] = previousRow[previousRow.Count - 1];
+                }
+                else
+                {
+                    row[i] = previousRow[i - 1] + previousRow[i];
+                }
+            }
+
+            cache.Add(rowIndex, row);
+
+            list.Add(row);
+
+            return list;
+        }
+    }
+}
---
+++ src/LeetCode-Exercises/Exercises/119-PascalsTriangleII.cs
@@ -0,0 +1,76 @@
+﻿using LeetCode.Attributes;
+using LeetCode.Extensions;
+using System;
+using System.Collections.Generic;
+
+namespace LeetCode.Exercises
+{
+    [Exercise]
+    public class PascalsTriangleII
+    {
+        public static void Run()
+        {
+            var ex = new PascalsTriangleII();
+
+            do
+            {
+                var n = ex.ReadNum("Enter row index");
+
+                if (n == null)
+                {
+                    break;
+                }
+
+                var row = ex.GetRow(n.Value);
+
+                Console.WriteLine(String.Join(", ", row));
+                Console.WriteLine();
+
+            } while (true);
+        }
+
+        static IDictionary<int, IList<int>> _cache = new Dictionary<int, IList<int>>();
+
+        public IList<int> GetRow(int rowIndex)
+        {
+            if (rowIndex < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(rowIndex));
+            }
+
+            if (rowIndex == 0)
+            {
+                return new[] { 1 };
+            }
+
+            if (_cache.ContainsKey(rowIndex))
+            {
+                return _cache[rowIndex];
+            }
+
+            var previousRow = GetRow(rowIndex - 1);
+
+            var row = new int[previousRow.Count + 1];
+
+            for (int i = 0; i < row.Length; i++)
+            {
+                if (i == 0)
+                {
+                    row[0] = previousRow[0];
+                }
+                else if (i == row.Length - 1)
+                {
+                    row[row.Length - 1] = previousRow[previousRow.Count - 1];
+                }
+                else
+                {
+                    row[i] = previousRow[i - 1] + previousRow[i];
+                }
+            }
+
+            _cache.Add(rowIndex, row);
+
+            return row;
+        }
+    }
+}
