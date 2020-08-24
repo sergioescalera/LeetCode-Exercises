59032cc3507f2472d49dc7d57b4da121e537a0fa
Sergio Escalera <sergio.escalera.c@gmail.com>
2020-08-24 10:34:56 -0400

    404 Sum of left leaves

---
+++ src/LeetCode-Exercises/Exercises/404-SumLeftLeaves.cs
@@ -0,0 +1,50 @@
+﻿using LeetCode.Attributes;
+using LeetCode.Input;
+
+namespace LeetCode.Exercises
+{
+    [Exercise]
+    public class SumLeftLeaves
+    {
+        public static void Run()
+        {
+            var ex = new SumLeftLeaves();
+
+            do
+            {
+                
+
+            } while (true);
+        }
+
+        public int SumOfLeftLeaves(TreeNode root)
+        {
+            if (root == null)
+            {
+                return 0;
+            }
+
+            if (root.left == null && root.right == null)
+            {
+                return 0;
+            }
+
+            return SumOfLeftLeaves(root.left, true) + SumOfLeftLeaves(root.right, false);
+        }
+
+        private int SumOfLeftLeaves(TreeNode node, bool isLeftNode)
+        {
+            if (node == null)
+            {
+                return 0;
+            }
+
+            if (node.left == null && node.right == null)
+            {
+                return isLeftNode ? node.val : 0;
+            }
+
+            return SumOfLeftLeaves(node.left, true) + SumOfLeftLeaves(node.right, false);
+        }
+    }
+}
