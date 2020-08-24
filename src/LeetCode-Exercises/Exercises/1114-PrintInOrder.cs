e9ddb87ac330e60fa7a74f87719e45408d25f0c6
Sergio Escalera <sergio.escalera.c@gmail.com>
2020-08-24 11:21:55 -0400

    1114. Print in Order

---
+++ src/LeetCode-Exercises/Exercises/1114-PrintInOrder.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading;
+
+namespace LeetCode.Exercises
+{
+    public class PrintInOrder
+    {
+
+    }
+
+    public class Foo
+    {
+        private CountdownEvent cd1;
+        private CountdownEvent cd2;
+
+        public Foo()
+        {
+            cd1 = new CountdownEvent(1);
+            cd2 = new CountdownEvent(1);
+        }
+
+        public void First(Action printFirst)
+        {
+            printFirst();
+
+            cd1.Signal();
+        }
+
+        public void Second(Action printSecond)
+        {
+            cd1.Wait();
+
+            printSecond();
+
+            cd2.Signal();
+        }
+
+        public void Third(Action printThird)
+        {
+            cd2.Wait();
+
+            printThird();
+        }
+    }
+}
