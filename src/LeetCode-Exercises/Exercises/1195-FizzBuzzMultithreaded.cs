6178b4909af9f049862579eef30dad77ca45c239
Sergio Escalera <sergio.escalera.c@gmail.com>
2020-08-27 10:33:30 -0400

    1195 Fizz buzz 436 Find right interval

---
+++ src/LeetCode-Exercises/Exercises/1195-FizzBuzzMultithreaded.cs
@@ -0,0 +1,183 @@
+﻿using LeetCode.Attributes;
+using LeetCode.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LeetCode.Exercises
+{
+    [Exercise]
+    public class FizzBuzzMultithreaded
+    {   
+        public static void Run()
+        {
+            do
+            {
+                var n = 0.ReadNum("Enter n");
+
+                if (n == null) break;
+
+                var fb = new FizzBuzz(n.Value);
+
+                var tasks = new Action[]
+                {
+                    () => fb.Buzz(() => Console.Write("Buzz ")),
+                    () => fb.Fizz(() => Console.Write("Fizz ")),
+                    () => fb.Fizzbuzz(() => Console.Write("Fizzbuzz ")),
+                    () => fb.Number((x) => Console.Write(x + " "))
+                }
+                .Select(o => Task.Factory.StartNew(o))
+                .ToArray();
+
+                Task.WaitAll(tasks);
+
+                Console.WriteLine();
+
+            } while (true);
+        }
+
+        /// <summary>
+        /// Implement a multithreaded version of FizzBuzz with four threads. 
+        /// The same instance of FizzBuzz will be passed to four different threads:
+        /// 
+        /// Thread A will call fizz() to check for divisibility of 3 and outputs fizz.
+        /// Thread B will call buzz() to check for divisibility of 5 and outputs buzz.
+        /// Thread C will call fizzbuzz() to check for divisibility of 3 and 5 and outputs fizzbuzz.
+        /// Thread D will call number() which should only output the numbers.
+        /// </summary>
+        public class FizzBuzz
+        {
+            private int n;
+            private int current = 1;
+
+            private SemaphoreSlim number = new SemaphoreSlim(0, 1);
+            private SemaphoreSlim fizz = new SemaphoreSlim(0, 1);
+            private SemaphoreSlim buzz = new SemaphoreSlim(0, 1);
+            private SemaphoreSlim fizzbuzz = new SemaphoreSlim(0, 1);
+
+            public FizzBuzz(int n)
+            {
+                this.n = n;
+
+                this.number.Release();
+            }
+
+            public void Fizz(Action printFizz)
+            {
+                do
+                {
+                    if (fizz.Wait(100))
+                    {
+                        if (current <= n && current % 3 == 0 && current % 5 != 0)
+                        {
+                            printFizz();
+
+                            current++;
+                        }
+
+                        ReleaseSemaphore();
+                    }
+
+                } while (current <= n);
+
+            }
+
+            public void Buzz(Action printBuzz)
+            {
+                do
+                {
+                    if (buzz.Wait(100))
+                    {
+                        if (current <= n && current % 5 == 0 && current % 3 != 0)
+                        {
+                            printBuzz();
+
+                            current++;
+                        }
+
+                        ReleaseSemaphore();
+                    }
+
+                } while (current <= n);
+            }
+
+            public void Fizzbuzz(Action printFizzBuzz)
+            {
+                do
+                {
+                    if (fizzbuzz.Wait(100))
+                    {
+                        if (current <= n && current % 3 == 0 && current % 5 == 0)
+                        {
+                            printFizzBuzz();
+
+                            current++;
+                        }
+
+                        ReleaseSemaphore();
+                    }
+
+                } while (current <= n);
+            }
+
+            public void Number(Action<int> printNumber)
+            {
+                do
+                {
+                    if (number.Wait(100))
+                    {
+                        if (current <= n && current % 3 != 0 && current % 5 != 0)
+                        {
+                            printNumber(current);
+
+                            current++;
+                        }
+
+                        ReleaseSemaphore();
+                    }
+
+                } while (current <= n);
+            }
+
+            private void ReleaseSemaphore()
+            {
+                if (current > n)
+                {
+                    new List<SemaphoreSlim>
+                    {
+                        fizz,
+                        buzz,
+                        fizzbuzz,
+                        number
+                    }
+                    .ForEach(s =>
+                    {
+                        if (s.CurrentCount == 0)
+                        {
+                            s.Release();
+                        }
+                    });
+                }
+
+                else if (current % 3 == 0 && current % 5 == 0)
+                {
+                    fizzbuzz.Release();
+                }
+                else if (current % 3 == 0)
+                {
+                    fizz.Release();
+                }
+                else if (current % 5 == 0)
+                {
+                    buzz.Release();
+                }
+                else
+                {
+                    number.Release();
+                }
+            }
+        }
+    }
+}
---
+++ src/LeetCode-Exercises/Exercises/436-FindRightInterval.cs
@@ -0,0 +1,105 @@
+﻿using LeetCode.Attributes;
+using System;
+using System.Linq;
+
+namespace LeetCode.Exercises
+{
+    [Exercise]
+    public class FindRightIntervalExercise
+    {
+        public static void Run()
+        {
+            var ex = new FindRightIntervalExercise();
+
+            var intervals = new[]
+            {
+                new [] { 3, 4 },
+                new [] { 2, 3 },
+                new [] { 1, 2 },
+                new [] { 0, 0 },
+                new [] { -1, -1 },
+                new [] { -4, -3 },
+                new [] { 5, 10 }
+            };
+
+            var right = ex.FindRightInterval(intervals);
+        }
+
+        /// <summary>
+        /// Given a set of intervals, for each of the interval i, check if there exists an interval j whose start point is bigger than or equal to the end point of the interval i, which can be called that j is on the "right" of i.
+        /// 
+        /// For any interval i, you need to store the minimum interval j's index, which means that the interval j has the minimum start point to build the "right" relationship for interval i. If the interval j doesn't exist, store -1 for the interval i.Finally, you need output the stored value of each interval as an array.
+        /// </summary>
+        /// <param name="intervals"></param>
+        /// <returns></returns>
+        public int[] FindRightInterval(int[][] intervals)
+        {
+            if (intervals == null)
+            {
+                throw new ArgumentNullException(nameof(intervals));
+            }
+
+            if (intervals.Length == 0)
+            {
+                return new int[0];
+            }
+
+            if (intervals.Length == 1)
+            {
+                return new[] { -1 };
+            }
+
+            var sorted = intervals
+                .Select((o, i) => new Interval
+                {
+                    index = i,
+                    start = o[0],
+                    end = o[1]
+                })
+                .OrderBy(o => o.start)
+                .ToArray();
+
+            var results = new int[intervals.Length];
+            
+            for (int i = 0; i < sorted.Length; i++)
+            {
+                var interval = sorted[i];
+
+                if (i == sorted.Length - 1)
+                {
+                    results[interval.index] = -1;     
+                }
+                else
+                {
+                    var next = FindNext(sorted, i);
+
+                    results[interval.index] = next == null ? -1 : next.index;
+                }
+            }
+
+            return results;
+        }
+
+        private Interval FindNext(Interval[] intervals, int i)
+        {
+            for (int j = i + 1; j < intervals.Length; j++)
+            {
+                var next = intervals[j];
+
+                if (next.start >= intervals[i].end)
+                {
+                    return next;
+                }
+            }
+
+            return null;
+        }
+
+        class Interval
+        {
+            internal int index;
+            internal int start;
+            internal int end;
+        }
+    }
+}
