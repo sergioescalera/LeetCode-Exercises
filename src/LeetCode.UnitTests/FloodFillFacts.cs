517e72f088750fcd232627e095cb3727a7ba01d8
Sergio Escalera <sergio.escalera.c@gmail.com>
2020-09-01 15:10:37 -0400

    733. Flood Fill

---
+++ src/LeetCode.UnitTests/FloodFillFacts.cs
@@ -0,0 +1,147 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace LeetCode.UnitTests
+{
+    [TestCategory("733. Flood Fill")]
+    [TestClass]
+    public class FloodFillFacts
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void ShouldThrowWhenNullImage()
+        {
+            var sol = new FloodFillSolution();
+
+            sol.FloodFill(null, 0, 0, 0);
+        }
+
+        [TestMethod]
+        public void TestCase1x1()
+        {
+            var sol = new FloodFillSolution();
+            var img = new[]
+            {
+                new [] { 1 }
+            };
+
+            sol.FloodFill(img, 0, 0, 2);
+
+            var expected = new[]
+            {
+                new [] { 2 }
+            };
+
+            Assert.AreEqual(expected.Length, img.Length);
+
+            for (int i = 0; i < expected.Length; i++)
+            {
+                CollectionAssert.AreEqual(expected[0], img[0]);
+            }
+        }
+
+        [TestMethod]
+        public void TestCase1x5()
+        {
+            var sol = new FloodFillSolution();
+            var img = new[]
+            {
+                new [] { 1, 1, 0, 2, 3 }
+            };
+
+            sol.FloodFill(img, 0, 0, 2);
+
+            var expected = new[]
+            {
+                new [] { 2, 2, 0, 2, 3 }
+            };
+
+            Assert.AreEqual(expected.Length, img.Length);
+
+            for (int i = 0; i < expected.Length; i++)
+            {
+                CollectionAssert.AreEqual(expected[0], img[0]);
+            }
+        }
+
+        [TestMethod]
+        public void TestCase2x2()
+        {
+            var sol = new FloodFillSolution();
+            var img = new[]
+            {
+                new [] { 1, 1 },
+                new [] { 1, 1 }
+            };
+
+            sol.FloodFill(img, 1, 1, 2);
+
+            var expected = new[]
+            {
+                new [] { 2, 2 },
+                new [] { 2, 2 }
+            };
+
+            Assert.AreEqual(expected.Length, img.Length);
+
+            for (int i = 0; i < expected.Length; i++)
+            {
+                CollectionAssert.AreEqual(expected[0], img[0]);
+            }
+        }
+
+        [TestMethod]
+        public void TestCase3x3()
+        {
+            var sol = new FloodFillSolution();
+            var img = new[]
+            {
+                new [] { 1, 1, 1 },
+                new [] { 1, 1, 0 },
+                new [] { 1, 0, 1 }
+            };
+            
+            sol.FloodFill(img, 1, 1, 2);
+
+            var expected = new[]
+            {
+                new [] { 2, 2, 2 },
+                new [] { 2, 2, 0 },
+                new [] { 2, 0, 1 }
+            };
+
+            Assert.AreEqual(expected.Length, img.Length);
+
+            for (int i = 0; i < expected.Length; i++)
+            {
+                CollectionAssert.AreEqual(expected[0], img[0]);
+            }
+        }
+
+        [TestMethod]
+        public void AvoidInfiniteLoop()
+        {
+            var sol = new FloodFillSolution();
+            var img = new[]
+            {
+                new [] { 0, 0, 0 },
+                new [] { 0, 1, 1 }
+            };
+
+            sol.FloodFill(img, 1, 1, 1);
+
+            var expected = new[]
+            {
+                new [] { 0, 0, 0 },
+                new [] { 0, 1, 1 }
+            };
+
+            Assert.AreEqual(expected.Length, img.Length);
+
+            for (int i = 0; i < expected.Length; i++)
+            {
+                CollectionAssert.AreEqual(expected[0], img[0]);
+            }
+        }
+    }
+}
---
+++ src/LeetCode/733-FloodFill.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace LeetCode
+{
+    public class FloodFillSolution
+    {
+        public int[][] FloodFill(int[][] image, int sr, int sc, int newColor)
+        {
+            if (image == null)
+            {
+                throw new ArgumentNullException(nameof(image));
+            }
+
+            if (sr >= image.Length)
+            {
+                return image;
+            }
+
+            if (sc >= image[sr].Length)
+            {
+                return image;
+            }
+
+            if (newColor == image[sr][sc])
+            {
+                return image;
+            }
+
+            return FloodFill(image, sr, sc, image[sr][sc], newColor);
+        }
+
+        private int[][] FloodFill(
+            int[][] image,
+            int sr, 
+            int sc,
+            int targetColor,
+            int newColor)
+        {
+            if (sr < 0 || sc < 0)
+            {
+                return image;
+            }
+
+            if (sr >= image.Length || sc >= image[sr].Length)
+            {
+                return image;
+            }
+
+            if (image[sr][sc] == targetColor)
+            {
+                image[sr][sc] = newColor;
+
+                FloodFill(image, sr + 1, sc, targetColor, newColor);
+                FloodFill(image, sr - 1, sc, targetColor, newColor);
+                FloodFill(image, sr, sc + 1, targetColor, newColor);
+                FloodFill(image, sr, sc - 1, targetColor, newColor);
+            }
+
+            return image;
+        }
+    }
+}
