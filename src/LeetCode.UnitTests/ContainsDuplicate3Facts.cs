f080bc3de6b5021499abf1f73e675ba0feeab336
Sergio Escalera <sergio.escalera.c@gmail.com>
2020-09-02 13:16:53 -0400

    "220. Contains Duplicate III - n * k

---
+++ src/LeetCode.UnitTests/ContainsDuplicate3Facts.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.UnitTests
+{
+    [TestClass]
+    [TestCategory("220. Contains Duplicate III")]
+    public class ContainsDuplicate3Facts
+    {
+        [TestMethod]
+        public void ShouldReturnFalseWhenNullNums()
+        {
+            var sol = new ContainsDuplicateIII();
+
+            Assert.IsFalse(sol.ContainsNearbyAlmostDuplicate(null, 1, 1));
+        }
+
+        [TestMethod]
+        public void ShouldReturnFalseWhenEmptyNums()
+        {
+            var sol = new ContainsDuplicateIII();
+
+            Assert.IsFalse(sol.ContainsNearbyAlmostDuplicate(new int[0], 1, 1));
+        }
+
+        [TestMethod]
+        public void ShouldReturnFalseWhenKIsLessOrEqualThan0()
+        {
+            var sol = new ContainsDuplicateIII();
+
+            Assert.IsFalse(sol.ContainsNearbyAlmostDuplicate(new int[] { 1, 1 }, 0, 0));
+
+            Assert.IsFalse(sol.ContainsNearbyAlmostDuplicate(new int[] { 1, 2 }, 0, 1));
+        }
+
+        [TestMethod]
+        public void ShouldReturnFalseWhenTIsLessThan0()
+        {
+            var sol = new ContainsDuplicateIII();
+
+            Assert.IsFalse(sol.ContainsNearbyAlmostDuplicate(new int[] { 1, 2 }, 1, -1));
+
+            Assert.IsFalse(sol.ContainsNearbyAlmostDuplicate(new int[] { 1, 1 }, 1, -1));
+        }
+
+        [TestMethod]
+        public void Example1ShouldReturnTrue()
+        {
+            var sol = new ContainsDuplicateIII();
+
+            Assert.IsTrue(sol.ContainsNearbyAlmostDuplicate(new int[] { 1, 2, 3, 1 }, 3, 0));
+        }
+
+        [TestMethod]
+        public void Example2ShouldReturnTrue()
+        {
+            var sol = new ContainsDuplicateIII();
+
+            Assert.IsTrue(sol.ContainsNearbyAlmostDuplicate(new int[] { 1, 0, 1, 1 }, 1, 2));
+        }
+
+        [TestMethod]
+        public void Example3ShouldReturnFalse()
+        {
+            var sol = new ContainsDuplicateIII();
+
+            Assert.IsFalse(sol.ContainsNearbyAlmostDuplicate(new int[] { 1, 5, 9, 1, 5, 9 }, 2, 3));
+        }
+
+        [TestMethod]
+        public void Example4ShouldReturnTrue()
+        {
+            var sol = new ContainsDuplicateIII();
+
+            Assert.IsTrue(sol.ContainsNearbyAlmostDuplicate(new int[] { 3, 6, 0, 4 }, 2, 2));
+        }
+
+        [TestMethod]
+        public void Example5ShouldNotOverflow()
+        {
+            var sol = new ContainsDuplicateIII();
+
+            Assert.IsFalse(sol.ContainsNearbyAlmostDuplicate(new int[] { -1, 2147483647 }, 1, 2147483647));
+        }
+    }
+}
---
+++ src/LeetCode/220-ContainsDuplicateIII.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    public class ContainsDuplicateIII
+    {
+        public bool ContainsNearbyAlmostDuplicate(int[] nums, int k, int t)
+        {
+            if (nums == null)
+            {
+                return false;
+            }
+
+            if (nums.Length <= 1)
+            {
+                return false;
+            }
+
+            if (k <= 0)
+            {
+                return false;
+            }
+
+            if (t < 0)
+            {
+                return false;
+            }
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = i + 1; j <= i + k && j < nums.Length; j++)
+                {
+                    var diff = Math.Abs((long)nums[i] - (long)nums[j]);
+
+                    if (diff <= t)
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            return false;
+        }
+    }
+}
