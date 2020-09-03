334f316bc0424fe93363dbd60508877496a92607
Sergio Escalera <sergio.escalera.c@gmail.com>
2020-09-02 20:54:33 -0400

    128. Longest Consecutive Sequence

---
+++ src/LeetCode.UnitTests/LongestConsecutiveSequenceFacts.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.UnitTests
+{
+    [TestClass]
+    [TestCategory("128. Longest Consecutive Sequence")]
+    public class LongestConsecutiveSequenceFacts
+    {
+        [TestMethod]
+        public void ShouldReturn0()
+        {
+            var sol = new LongestConsecutiveSequence();
+
+            Assert.AreEqual(0, sol.LongestConsecutive(new int[0]));
+
+            Assert.AreEqual(0, sol.LongestConsecutive(null));
+        }
+
+        [TestMethod]
+        public void ShouldReturn1()
+        {
+            var sol = new LongestConsecutiveSequence();
+
+            Assert.AreEqual(1, sol.LongestConsecutive(new[] { 0 }));
+
+            Assert.AreEqual(1, sol.LongestConsecutive(new[] { 1 }));
+
+            Assert.AreEqual(1, sol.LongestConsecutive(new[] { 10 }));
+
+            Assert.AreEqual(1, sol.LongestConsecutive(new[] { int.MaxValue }));
+
+            Assert.AreEqual(1, sol.LongestConsecutive(new[] { 1, 1, 1 }));
+
+            Assert.AreEqual(1, sol.LongestConsecutive(new[] { 0, 0 }));
+        }
+
+        [TestMethod]
+        public void ShouldReturn4()
+        {
+            var sol = new LongestConsecutiveSequence();
+
+            Assert.AreEqual(4, sol.LongestConsecutive(new[] { 100, 4, 200, 1, 3, 2, 1, 2, 4 }));
+        }
+    }
+}
---
+++ src/LeetCode/128-LongestConsecutiveSequence.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode
+{
+    public class LongestConsecutiveSequence
+    {
+        public int LongestConsecutive(int[] nums)
+        {
+            if (nums == null)
+            {
+                return 0;
+            }
+
+            if (nums.Length == 0)
+            {
+                return 0;
+            }
+
+            var set = new HashSet<int>(nums);
+            
+            var longest = 1;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                var n = nums[i];
+                var count = 1;
+
+                if (set.Contains(n - 1))
+                {
+                    continue;
+                }
+
+                if (set.Contains(n) == false)
+                {
+                    continue;
+                }
+
+                while (set.Contains(n + count))
+                {
+                    count++;
+                }
+
+                longest = Math.Max(count, longest);
+            }
+
+            return longest;
+        }
+    }
+}
