7ac72d4c05f0a8edf2dd71e48516673064314cf5
Sergio Escalera <sergio.escalera.c@gmail.com>
2020-09-02 21:46:17 -0400

    766. Toeplitz Matrix

---
+++ src/LeetCode.UnitTests/ToeplitzMatrixFacts.cs
@@ -0,0 +1,145 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.UnitTests
+{
+    [TestClass]
+    [TestCategory("766. Toeplitz Matrix")]
+    public class ToeplitzMatrixFacts
+    {
+        [TestMethod]
+        public void ShouldReturnTrue1x1()
+        {
+            var sol = new ToeplitzMatrix();
+
+            Assert.IsTrue(sol.IsToeplitzMatrix(new[]
+            {
+                new[] { 1 }
+            }));
+
+            Assert.IsTrue(sol.IsToeplitzMatrix(new[]
+            {
+                new[] { int.MinValue }
+            }));
+        }
+
+        [TestMethod]
+        public void ShouldReturnFalse2x2()
+        {
+            var sol = new ToeplitzMatrix();
+
+            Assert.IsFalse(sol.IsToeplitzMatrix(new[]
+            {
+                new[] { 1, 2 },
+                new[] { 2, 2 }
+            }));
+
+            Assert.IsFalse(sol.IsToeplitzMatrix(new[]
+            {
+                new[] { 1, 3 },
+                new[] { 2, 2 }
+            }));
+        }
+
+        [TestMethod]
+        public void ShouldReturnTrue2x2()
+        {
+            var sol = new ToeplitzMatrix();
+
+            Assert.IsTrue(sol.IsToeplitzMatrix(new[]
+            {
+                new[] { 1, 2 },
+                new[] { 2, 1 }
+            }));
+
+            Assert.IsTrue(sol.IsToeplitzMatrix(new[]
+            {
+                new[] { 1, 3 },
+                new[] { 2, 1 }
+            }));
+        }
+
+        [TestMethod]
+        public void ShouldReturnTrue3x2()
+        {
+            var sol = new ToeplitzMatrix();
+
+            Assert.IsTrue(sol.IsToeplitzMatrix(new[]
+            {
+                new[] { 1, 2 },
+                new[] { 2, 1 },
+                new[] { 4, 2 }
+            }));
+        }
+
+        [TestMethod]
+        public void ShouldReturnFalse3x2()
+        {
+            var sol = new ToeplitzMatrix();
+
+            Assert.IsFalse(sol.IsToeplitzMatrix(new[]
+            {
+                new[] { 2, 2 },
+                new[] { 2, 1 },
+                new[] { 4, 2 }
+            }));
+
+            Assert.IsFalse(sol.IsToeplitzMatrix(new[]
+            {
+                new[] { 1, 2 },
+                new[] { 2, 1 },
+                new[] { 4, 3 }
+            }));
+        }
+
+        [TestMethod]
+        public void ShouldReturnTrue4x3()
+        {
+            var sol = new ToeplitzMatrix();
+
+            var matrix = new[]
+            {
+                new[] { 1, 2, 3, 4 },
+                new[] { 5, 1, 2, 3 },
+                new[] { 9, 5, 1, 2 }
+            };
+
+            var result = sol.IsToeplitzMatrix(matrix);
+
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void ShouldReturnFalse4x3()
+        {
+            var sol = new ToeplitzMatrix();
+
+            Assert.IsFalse(sol.IsToeplitzMatrix(new[]
+            {
+                new[] { 1, 2, 3, 4 },
+                new[] { 5, 1, 2, 3 },
+                new[] { 9, 5, 2, 2 }
+            }));
+
+            Assert.IsFalse(sol.IsToeplitzMatrix(new[]
+            {
+                new[] { 1, 2, 3, 4 },
+                new[] { 5, 1, 5, 3 },
+                new[] { 9, 5, 1, 2 }
+            }));
+
+            Assert.IsFalse(sol.IsToeplitzMatrix(new[]
+            {
+                new[] { 1, 2, 3, 4 },
+                new[] { 5, 1, 2, 4 },
+                new[] { 9, 5, 1, 2 }
+            }));
+
+            Assert.IsFalse(sol.IsToeplitzMatrix(new[]
+            {
+                new[] { 1, 2, 3, 4 },
+                new[] { 5, 1, 2, 3 },
+                new[] { 9, 6, 1, 2 }
+            }));
+        }
+    }
+}
---
+++ src/LeetCode/766-ToeplitzMatrix.cs
@@ -0,0 +1,56 @@
+﻿namespace LeetCode
+{
+    public class ToeplitzMatrix
+    {
+        public bool IsToeplitzMatrix(int[][] matrix)
+        {
+            if (matrix == null)
+            {
+                return false;
+            }
+
+            if (matrix.Length == 0)
+            {
+                return false;
+            }
+
+            if (matrix.Length == 1)
+            {
+                return true;
+            }
+
+            var n = matrix.Length;
+            var m = matrix[0].Length;
+
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n - 1 && j + i < m - 1; j++)
+                {
+                    var x = matrix[j][j + i];
+                    var y = matrix[j + 1][j + i + 1];
+
+                    if (x != y)
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m - 1 && i + j < n - 1; j++)
+                {
+                    var x = matrix[j + i][j];
+                    var y = matrix[j + i + 1][j + 1];
+
+                    if (x != y)
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+    }
+}
