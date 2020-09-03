11a6f6ec72889c2adc5e6903db996a0b37869e82
Sergio Escalera <sergio.escalera.c@gmail.com>
2020-09-03 18:41:12 -0400

    1460. Make Two Arrays Equal by Reversing Sub-arrays

---
+++ src/LeetCode.UnitTests/MakeTwoArraysEqualByReversingSubArraysFacts.cs
@@ -0,0 +1,100 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.UnitTests
+{
+    [TestClass]
+    [TestCategory("1460. Make Two Arrays Equal by Reversing Sub-arrays")]
+    public class MakeTwoArraysEqualByReversingSubArraysFacts
+    {
+        [TestMethod]
+        public void Example1()
+        {
+            var sol = new MakeTwoArraysEqualByReversingSubArrays();
+
+            var result = sol.CanBeEqual
+                (
+                    new[] { 1, 2, 3, 4 },
+                    new[] { 2, 4, 1, 3 }
+                );
+
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void Example2()
+        {
+            var sol = new MakeTwoArraysEqualByReversingSubArrays();
+
+            Assert.IsTrue(sol.CanBeEqual
+                (
+                    new[] { 7 },
+                    new[] { 7 }
+                ));
+
+            Assert.IsFalse(sol.CanBeEqual
+                (
+                    new[] { 7 },
+                    new[] { 8 }
+                ));
+        }
+
+        [TestMethod]
+        public void Example3()
+        {
+            var sol = new MakeTwoArraysEqualByReversingSubArrays();
+
+            Assert.IsTrue(sol.CanBeEqual
+                (
+                    new[] { 1, 12 },
+                    new[] { 12, 1 }
+                ));
+
+            Assert.IsFalse(sol.CanBeEqual
+                (
+                    new[] { 7, 9 },
+                    new[] { 8, 7 }
+                ));
+        }
+
+        [TestMethod]
+        public void Example4()
+        {
+            var sol = new MakeTwoArraysEqualByReversingSubArrays();
+
+            Assert.IsFalse(sol.CanBeEqual
+                (
+                    new[] { 3, 7, 9 },
+                    new[] { 3, 7, 11 }
+                ));
+
+            Assert.IsTrue(sol.CanBeEqual
+                (
+                    new[] { 3, 7, 11 },
+                    new[] { 3, 7, 11 }
+                ));
+        }
+
+        [TestMethod]
+        public void Example5()
+        {
+            var sol = new MakeTwoArraysEqualByReversingSubArrays();
+
+            Assert.IsTrue(sol.CanBeEqual
+                (
+                    new[] { 1, 1, 1, 1, 1 },
+                    new[] { 1, 1, 1, 1, 1 }
+                ));
+
+            Assert.IsFalse(sol.CanBeEqual
+                (
+                    new[] { 1, 1, 1, 1, 1 },
+                    new[] { 1, 1, 1, 1, 2 }
+                ));
+        }
+    }
+}
---
+++ src/LeetCode/1460-MakeTwoArraysEqualByReversingSubArrays.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode
+{
+    public class MakeTwoArraysEqualByReversingSubArrays
+    {
+        public bool CanBeEqual(int[] arr_a, int[] arr_b)
+        {
+            if (arr_a == null || arr_b == null)
+            {
+                return false;
+            }
+
+            if (arr_a.Length == 0|| arr_b.Length == 0)
+            {
+                return false;
+            }
+
+            if (arr_a.Length != arr_b.Length)
+            {
+                return false;
+            }
+
+            var dict_b = ToDictionary(arr_b, (i) => false);
+            var dict_a = ToDictionary(arr_a, (i) => dict_b.ContainsKey(i) == false);
+
+            if (dict_b.Keys.Count != dict_a.Keys.Count)
+            {
+                return false;
+            }
+
+            foreach (var key in dict_b.Keys)
+            {
+                if (dict_a.ContainsKey(key) == false)
+                {
+                    return false;
+                }
+
+                if (dict_b[key] != dict_a[key])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        private IDictionary<int, int> ToDictionary(int[] array, Func<int, bool> stop)
+        {
+            var dict = new Dictionary<int, int>();
+
+            foreach (var item in array)
+            {
+                if (stop(item))
+                {
+                    break;
+                }
+
+                if (dict.ContainsKey(item))
+                {
+                    dict[item]++;
+                }
+                else
+                {
+                    dict.Add(item, 1);
+                }
+            }
+
+            return dict;
+        }
+    }
+}
