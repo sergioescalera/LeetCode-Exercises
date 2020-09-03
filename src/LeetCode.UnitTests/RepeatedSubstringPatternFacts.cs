eb729a1bdc419db5b051d5095ff314888b037053
Sergio Escalera <sergio.escalera.c@gmail.com>
2020-09-03 12:34:18 -0400

    459. Repeated Substring Pattern

---
+++ src/LeetCode.UnitTests/RepeatedSubstringPatternFacts.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace LeetCode.UnitTests
+{
+    [TestClass]
+    [TestCategory("459. Repeated Substring Pattern")]
+    public class RepeatedSubstringPatternFacts
+    {
+        [TestMethod]
+        public void ShouldReturnFalseWhenNullOrEmpty()
+        {
+            var sol = new RepeatedSubstringPatternSolution();
+
+            Assert.IsFalse(sol.RepeatedSubstringPattern(null));
+            Assert.IsFalse(sol.RepeatedSubstringPattern(String.Empty));
+        }
+
+        [TestMethod]
+        public void ShouldReturnFalseWhenOneLetter()
+        {
+            var sol = new RepeatedSubstringPatternSolution();
+
+            Assert.IsFalse(sol.RepeatedSubstringPattern("a"));
+            Assert.IsFalse(sol.RepeatedSubstringPattern("z"));
+        }
+
+        [TestMethod]
+        public void TwoLetters()
+        {
+            var sol = new RepeatedSubstringPatternSolution();
+
+            Assert.IsTrue(sol.RepeatedSubstringPattern("aa"));
+            Assert.IsTrue(sol.RepeatedSubstringPattern("zz"));
+            Assert.IsFalse(sol.RepeatedSubstringPattern("ab"));
+            Assert.IsFalse(sol.RepeatedSubstringPattern("xy"));
+        }
+
+        [TestMethod]
+        public void ThreeLetters()
+        {
+            var sol = new RepeatedSubstringPatternSolution();
+
+            Assert.IsTrue(sol.RepeatedSubstringPattern("aaa"));
+            Assert.IsTrue(sol.RepeatedSubstringPattern("bbb"));
+            Assert.IsFalse(sol.RepeatedSubstringPattern("zaz"));
+            Assert.IsFalse(sol.RepeatedSubstringPattern("abc"));
+            Assert.IsFalse(sol.RepeatedSubstringPattern("xxy"));
+            Assert.IsFalse(sol.RepeatedSubstringPattern("xyy"));
+        }
+
+        [TestMethod]
+        public void FourLetters()
+        {
+            var sol = new RepeatedSubstringPatternSolution();
+
+            Assert.IsTrue(sol.RepeatedSubstringPattern("aaaa"));
+            Assert.IsFalse(sol.RepeatedSubstringPattern("bbaa"));
+            Assert.IsTrue(sol.RepeatedSubstringPattern("zaza"));
+            Assert.IsFalse(sol.RepeatedSubstringPattern("aaab"));
+            Assert.IsFalse(sol.RepeatedSubstringPattern("abcd"));
+            Assert.IsFalse(sol.RepeatedSubstringPattern("xxyz"));
+            Assert.IsFalse(sol.RepeatedSubstringPattern("xyyz"));
+        }
+
+        [TestMethod]
+        public void FiveLetters()
+        {
+            var sol = new RepeatedSubstringPatternSolution();
+
+            Assert.IsTrue(sol.RepeatedSubstringPattern("aaaaa"));
+            Assert.IsFalse(sol.RepeatedSubstringPattern("bbaaa"));
+            Assert.IsFalse(sol.RepeatedSubstringPattern("zazza"));
+            Assert.IsFalse(sol.RepeatedSubstringPattern("aaaab"));
+            Assert.IsFalse(sol.RepeatedSubstringPattern("abcde"));
+            Assert.IsFalse(sol.RepeatedSubstringPattern("xxyzz"));
+            Assert.IsFalse(sol.RepeatedSubstringPattern("xyyzz"));
+        }
+    }
+}
---
+++ src/LeetCode/459-Repeated SubstringPattern.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace LeetCode
+{
+    public class RepeatedSubstringPatternSolution
+    {
+        public bool RepeatedSubstringPattern(string s)
+        {
+            if (String.IsNullOrEmpty(s))
+            {
+                return false;
+            }
+
+            if (s.Length == 1)
+            {
+                return false;
+            }
+
+            var half = s.Length / 2;
+
+            for (int len = 1; len <= half; len++)
+            {
+                if (s.Length % len != 0)
+                {
+                    continue;
+                }
+
+                var pattern = s.Substring(0, len);
+
+                if (CheckPattern(s, pattern))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        private bool CheckPattern(string s, string pattern)
+        {
+            var count = s.Length / pattern.Length;
+
+            for (int skip = 1; skip < count; skip++)
+            {
+                if (pattern != s.Substring(skip * pattern.Length, pattern.Length))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
