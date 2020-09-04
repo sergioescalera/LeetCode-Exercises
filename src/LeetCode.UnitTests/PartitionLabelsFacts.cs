90e9a6d2e42e41eb925be1610bc90ea7fd98d9f9
Sergio Escalera <sergio.escalera.c@gmail.com>
2020-09-04 15:09:05 -0400

    763. Partition Labels

---
+++ src/LeetCode.UnitTests/PartitionLabelsFacts.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+
+namespace LeetCode.UnitTests
+{
+    [TestClass]
+    [TestCategory("763. Partition Labels")]
+    public class PartitionLabelsFacts
+    {
+        [TestMethod]
+        public void Example1()
+        {
+            var sol = new PartitionLabelsSolution();
+
+            CollectionAssert.AreEquivalent(new int[0], sol.PartitionLabels(null).ToList());
+            CollectionAssert.AreEquivalent(new int[0], sol.PartitionLabels("").ToList());
+
+            CollectionAssert.AreEquivalent(
+                new int[] { 1 },
+                sol.PartitionLabels("a").ToList());
+
+            CollectionAssert.AreEquivalent(
+                new int[] { 1, 1 },
+                sol.PartitionLabels("ab").ToList());
+
+            CollectionAssert.AreEquivalent(
+                new int[] { 1, 1, 1 },
+                sol.PartitionLabels("abc").ToList());
+
+            CollectionAssert.AreEquivalent(
+                new int[] { 4 },
+                sol.PartitionLabels("abca").ToList());
+        }
+
+        [TestMethod]
+        public void Example2()
+        {
+            var sol = new PartitionLabelsSolution();
+
+            CollectionAssert.AreEquivalent(
+                new int[] { 6 },
+                sol.PartitionLabels("abcabc").ToList());
+
+            CollectionAssert.AreEquivalent(
+                new int[] { 9, 7, 8 },
+                sol.PartitionLabels("ababcbacadefegdehijhklij").ToList());
+        }
+    }
+}
---
+++ src/LeetCode/763-PartitionLabels.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+namespace LeetCode
+{
+    public class PartitionLabelsSolution
+    {
+        public IList<int> PartitionLabels(string str)
+        {
+            if (str == null || str.Length == 0)
+            {
+                return new int[0];
+            }
+
+            var partitions = new List<int>(str.Length);
+
+            var intervals = new SortedDictionary<char, Interval>();
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                var x = str[i];
+
+                if (intervals.ContainsKey(x))
+                {
+                    intervals[x].end = i;
+                }
+                else 
+                {
+                    intervals.Add(x, new Interval
+                    {
+                        start = i,
+                        end = i
+                    });
+                }
+            }
+
+            if (intervals.Count == 1)
+            {
+                return new[] { str.Length };
+            }
+
+            if (intervals.Count == str.Length)
+            {
+                return str.Select(o => 1).ToArray();
+            }
+
+            var set = new SortedSet<Interval>(intervals.Values);
+
+            while (set.Count > 0)
+            {
+                var current = set.Min;
+
+                set.Remove(current);
+
+                while (set.Count > 0 && set.Min.start < current.end)
+                {
+                    var next = set.Min;
+
+                    set.Remove(next);
+
+                    current.end = Math.Max(next.end, current.end);
+                }
+
+                partitions.Add(current.end - current.start + 1);
+            }
+
+            return partitions;
+        }
+
+        [DebuggerDisplay("[{start} - {end}]")]
+        class Interval : IComparable<Interval>
+        {
+            public int start;
+            public int end;
+
+            public override int GetHashCode()
+            {
+                return start.GetHashCode();
+            }
+
+            public override bool Equals(object obj)
+            {
+                return obj is Interval i && i.start == start && i.end == end;
+            }
+
+            public int CompareTo(Interval other)
+            {
+                return start - other.start;
+            }
+        }
+
+        class IntervalComparer : IComparer<Interval>
+        {
+            public int Compare(Interval x, Interval y)
+            {
+                return x.start - y.start;
+            }
+        }
+    }
+}
