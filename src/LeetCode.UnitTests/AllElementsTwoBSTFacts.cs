1f650058b090eac11b00ce8c9c070cb2b141efcb
Sergio Escalera <sergio.escalera.c@gmail.com>
2020-09-05 10:59:06 -0400

    1305. All Elements in Two Binary Search Trees

---
+++ src/LeetCode.UnitTests/AllElementsTwoBSTFacts.cs
@@ -0,0 +1,96 @@
+﻿using LeetCode.Data;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+
+namespace LeetCode.UnitTests
+{
+    [TestClass]
+    [TestCategory("1305. All Elements in Two Binary Search Trees")]
+    public class AllElementsTwoBSTFacts
+    {
+        [TestMethod]
+        public void EmptyTrees()
+        {
+            var sol = new AllElementsTwoBST();
+
+            var merged = sol.GetAllElements(null, null);
+
+            Assert.IsNotNull(merged);
+            Assert.IsFalse(merged.Any());
+        }
+
+        [TestMethod]
+        public void EmptyTree()
+        {
+            var sol = new AllElementsTwoBST();
+
+            CollectionAssert.AreEqual(
+                new[] { -99 },
+                sol.GetAllElements(new TreeNode(-99), null).ToArray());
+
+            CollectionAssert.AreEqual(
+                new[] { 99 },
+                sol.GetAllElements(null, new TreeNode(99)).ToArray());
+
+            CollectionAssert.AreEqual(
+                new[] { -10, 0, 10 },
+                sol.GetAllElements(
+                    null, 
+                    new TreeNode(0, new TreeNode(-10), new TreeNode(10))).ToArray());
+
+            CollectionAssert.AreEqual(
+                new[] { -10, 0, 10 },
+                sol.GetAllElements(
+                    new TreeNode(0, new TreeNode(-10), new TreeNode(10)),
+                    null).ToArray());
+        }
+
+        [TestMethod]
+        public void SameElements()
+        {
+            var sol = new AllElementsTwoBST();
+
+            CollectionAssert.AreEqual(
+                new[] { 1, 1, 8, 8 },
+                sol.GetAllElements(
+                new TreeNode(1, null, new TreeNode(8)),
+                new TreeNode(8, new TreeNode(1), null)).ToList());
+        }
+
+        [TestMethod]
+        public void DiffElements()
+        {
+            var sol = new AllElementsTwoBST();
+
+            CollectionAssert.AreEqual(
+                new[] { 1, 2, 8, 9, 10 },
+                sol.GetAllElements(
+                    new TreeNode(1, null, new TreeNode(9)),
+                    new TreeNode(8, new TreeNode(2), new TreeNode(10))).ToArray());
+
+            CollectionAssert.AreEqual(
+                new[] { 1, 2, 8, 9, 10 },
+                sol.GetAllElements(
+                    new TreeNode(8, new TreeNode(2), new TreeNode(10)),
+                    new TreeNode(1, null, new TreeNode(9))).ToArray());
+
+            CollectionAssert.AreEqual(
+                new[] { 0, 1, 1, 2, 3, 4 },
+                sol.GetAllElements(
+                    new TreeNode(2, new TreeNode(1), new TreeNode(4)),
+                    new TreeNode(1, new TreeNode(0), new TreeNode(3))).ToArray());
+
+            CollectionAssert.AreEqual(
+                new[] { -10, 0, 0, 1, 2, 5, 7, 10 },
+                sol.GetAllElements(
+                    new TreeNode(0, new TreeNode(-10), new TreeNode(10)),
+                    new TreeNode(5, new TreeNode(1, new TreeNode(0), new TreeNode(2)), new TreeNode(7))).ToArray());
+
+            CollectionAssert.AreEqual(
+                new[] { -10, 0, 0, 1, 2, 5, 7, 10 },
+                sol.GetAllElements(
+                    new TreeNode(5, new TreeNode(1, new TreeNode(0), new TreeNode(2)), new TreeNode(7)),
+                    new TreeNode(0, new TreeNode(-10), new TreeNode(10))).ToArray());
+        }
+    }
+}
---
+++ src/LeetCode/1305-AllElementsTwoBST.cs
@@ -0,0 +1,65 @@
+﻿using LeetCode.Data;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode
+{
+    public class AllElementsTwoBST
+    {
+        public IList<int> GetAllElements(TreeNode root1, TreeNode root2)
+        {
+            var list1 = Enumarate(root1).ToArray();
+            var list2 = Enumarate(root2).ToArray();
+            var index1 = 0;
+            var index2 = 0;
+
+            var list = new List<int>(
+                list1.Length + 
+                list2.Length);
+
+            while (index1 < list1.Length 
+                || index2 < list2.Length)
+            {
+                if (index1 < list1.Length && 
+                   (index2 >= list2.Length || list1[index1] <= list2[index2]))
+                {
+                    list.Add(list1[index1]);
+                    index1++;
+                }
+                else
+                {
+                    list.Add(list2[index2]);
+                    index2++;
+                }
+            }
+
+            return list;
+        }
+
+        private IEnumerable<int> Enumarate(TreeNode node)
+        {
+            if (node?.left != null)
+            {
+                foreach (var item in Enumarate(node.left))
+                {
+                    yield return item;
+                }
+            }
+
+            if (node != null)
+            {
+                yield return node.val;
+            }
+
+            if (node?.right != null)
+            {
+                foreach (var item in Enumarate(node.right))
+                {
+                    yield return item;
+                }
+            }
+
+            yield break;
+        }
+    }
+}
