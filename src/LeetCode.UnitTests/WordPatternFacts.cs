4b8581620a55cc960a094e4820ee31af3df7903b
Sergio Escalera <sergio.escalera.c@gmail.com>
2020-09-07 10:11:24 -0400

    290 - Word pattern

---
+++ src/LeetCode.UnitTests/WordPatternFacts.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.UnitTests
+{
+    [TestClass]
+    [TestCategory("290. Word Pattern")]
+    public class WordPatternFacts
+    {
+        [TestMethod]
+        public void Example1()
+        {
+            var sol = new WordPatternSolution();
+
+            Assert.IsTrue(sol.WordPattern("abba", "dog cat cat dog"));
+        }
+
+        [TestMethod]
+        public void Example2()
+        {
+            var sol = new WordPatternSolution();
+
+            Assert.IsFalse(sol.WordPattern("abba", "dog cat cat fish"));
+        }
+
+        [TestMethod]
+        public void Example3()
+        {
+            var sol = new WordPatternSolution();
+
+            Assert.IsFalse(sol.WordPattern("aaaa", "dog cat cat dog"));
+        }
+
+
+        [TestMethod]
+        public void Example4()
+        {
+            var sol = new WordPatternSolution();
+
+            Assert.IsFalse(sol.WordPattern("abba", "dog dog dog dog"));
+        }
+    }
+}
---
+++ src/LeetCode/0290-WordPattern.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode
+{
+    public class WordPatternSolution
+    {
+        public bool WordPattern(string pattern, string str)
+        {
+            if (String.IsNullOrEmpty(pattern) ||
+                String.IsNullOrEmpty(str))
+            {
+                return false;
+            }
+
+            var words = str.Split(' ');
+
+            if (words.Length != pattern.Length)
+            {
+                return false;
+            }
+
+            var map1 = new Dictionary<char, String>();
+            var map2 = new Dictionary<String, char>();
+
+            for (int i = 0; i < pattern.Length; i++)
+            {
+                var c = pattern[i];
+
+                var word = words[i];
+
+                var known1 = map1.ContainsKey(c);
+
+                var known2 = map2.ContainsKey(word);
+
+                if (known1 != known2)
+                {
+                    return false;
+                }
+
+                if (known1 && (map1[c] != word || map2[word] != c))
+                {
+                    return false;
+                }
+
+                if (!known1)
+                {
+                    map1.Add(c, word);
+
+                    map2.Add(word, c);
+                }
+            }
+
+            return true;
+        }
+    }
+}
