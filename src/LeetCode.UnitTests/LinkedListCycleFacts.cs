dec672ed733c893160941d515591dcede02572cd
Sergio Escalera <sergio.escalera.c@gmail.com>
2020-09-07 16:19:10 -0400

    0141. Linked List Cycle

---
+++ src/LeetCode.UnitTests/LinkedListCycleFacts.cs
@@ -0,0 +1,64 @@
+﻿using LeetCode.Data;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.UnitTests
+{
+    [TestCategory("0141. Linked List Cycle")]
+    [TestClass]
+    public class LinkedListCycleFacts
+    {
+        [TestMethod]
+        public void OneElem()
+        {
+            var sol = new LinkedListCycle();
+
+            var head = new ListNode(1);
+
+            Assert.IsFalse(sol.HasCycle(head));
+
+            head.next = head;
+
+            Assert.IsTrue(sol.HasCycle(head));
+        }
+
+        [TestMethod]
+        public void TwoElem()
+        {
+            var sol = new LinkedListCycle();
+
+            var head = new ListNode(1, new ListNode(2));
+
+            Assert.IsFalse(sol.HasCycle(head));
+
+            head.next.next = head;
+
+            Assert.IsTrue(sol.HasCycle(head));
+
+            head.next.next = head.next;
+
+            Assert.IsTrue(sol.HasCycle(head));
+        }
+
+        [TestMethod]
+        public void ThreeElem()
+        {
+            var sol = new LinkedListCycle();
+
+            var head = new ListNode(1, new ListNode(2, new ListNode(3)));
+
+            Assert.IsFalse(sol.HasCycle(head));
+
+            head.next.next.next = head;
+
+            Assert.IsTrue(sol.HasCycle(head));
+
+            head.next.next.next = head.next;
+
+            Assert.IsTrue(sol.HasCycle(head));
+
+            head.next.next.next = head.next.next;
+
+            Assert.IsTrue(sol.HasCycle(head));
+        }
+    }
+}
---
+++ src/LeetCode/0141-LinkedListCycle.cs
@@ -0,0 +1,60 @@
+﻿using LeetCode.Data;
+using System.Collections.Generic;
+
+namespace LeetCode
+{
+    public class LinkedListCycle
+    {
+        public bool HasCycle(ListNode head)
+        {
+            if (head == null)
+            {
+                return false;
+            }
+
+            var slow = head;
+            var fast = head;
+
+            while (slow.next != null
+                && fast.next != null
+                && fast.next.next != null)
+            {
+                slow = slow.next;
+                fast = fast.next.next;
+
+                if (slow == fast)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        bool HasCycle(ListNode node, Dictionary<int, List<ListNode>> memory)
+        {
+            if (node == null)
+            {
+                return false;
+            }
+
+            if (memory.ContainsKey(node.val))
+            {
+                var list = memory[node.val];
+
+                if (list.Contains(node))
+                {
+                    return true;
+                }
+
+                list.Add(node);
+            }
+            else
+            {
+                memory.Add(node.val, new List<ListNode>(new[] { node }));
+            }
+
+            return HasCycle(node.next, memory);
+        }
+    }
+}
