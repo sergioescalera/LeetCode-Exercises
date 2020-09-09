cd6f041c14c1c1c15a4fd474e0a949bcfa99df6c
Sergio Escalera <sergio.escalera.c@gmail.com>
2020-09-09 10:35:43 -0400

    0165. Compare Version Numbers

---
+++ src/LeetCode.UnitTests/CompareVersionNumbersFacts.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.UnitTests
+{
+    [TestClass]
+    [TestCategory("0165. Compare Version Numbers")]
+    public class CompareVersionNumbersFacts
+    {
+        [TestMethod]
+        public void Example1()
+        {
+            var sol = new CompareVersionNumbers();
+
+            Assert.AreEqual(-1, sol.CompareVersion("0.1", "1.1"));
+        }
+
+        [TestMethod]
+        public void Example2()
+        {
+            var sol = new CompareVersionNumbers();
+
+            Assert.AreEqual(1, sol.CompareVersion("1.0.1", "1"));
+        }
+
+        [TestMethod]
+        public void Example3()
+        {
+            var sol = new CompareVersionNumbers();
+
+            Assert.AreEqual(-1, sol.CompareVersion("7.5.2.4", "7.5.3"));
+        }
+
+        [TestMethod]
+        public void Example4()
+        {
+            var sol = new CompareVersionNumbers();
+
+            Assert.AreEqual(0, sol.CompareVersion("1.01", "1.001"));
+        }
+
+        [TestMethod]
+        public void Example5()
+        {
+            var sol = new CompareVersionNumbers();
+
+            Assert.AreEqual(0, sol.CompareVersion("1.0", "1.0.0"));
+        }
+
+        [TestMethod]
+        public void Example6()
+        {
+            var sol = new CompareVersionNumbers();
+
+            Assert.AreEqual(-1, sol.CompareVersion("2.9.9.9", "10.0.0"));
+        }
+    }
+}
---
+++ src/LeetCode/0165-CompareVersionNumbers.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    public class CompareVersionNumbers
+    {
+        public int CompareVersion(string version1, string version2)
+        {
+            if (string.IsNullOrEmpty(version1))
+            {
+                return 1;
+            }
+
+            if (string.IsNullOrEmpty(version2))
+            {
+                return -1;
+            }
+
+            var numbers1 = version1.Split('.');
+            var numbers2 = version2.Split('.');
+            var len = Math.Max(numbers1.Length, numbers2.Length);
+
+            for (int i = 0; i < len; i++)
+            {
+                var str1 = i < numbers1.Length ? numbers1[i] : "0";
+                var str2 = i < numbers2.Length ? numbers2[i] : "0";
+                
+                if (str1 == str2)
+                {
+                    continue;
+                }
+
+                var num1 = int.Parse(str1);
+                var num2 = int.Parse(str2);
+
+                if (num1 < num2)
+                {
+                    return -1;
+                }
+
+                if (num1 > num2)
+                {
+                    return 1;
+                }
+            }
+
+            return 0;
+        }
+    }
+}
