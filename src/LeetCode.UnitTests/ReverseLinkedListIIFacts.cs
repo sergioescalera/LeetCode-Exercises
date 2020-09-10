ed3441f71dbc1912531429d3d4489e0d64ba2910
Sergio Escalera <sergio.escalera.c@gmail.com>
2020-09-10 15:10:57 -0400

    92. Reverse Linked List II

---
+++ src/LeetCode.UnitTests/ReverseLinkedListIIFacts.cs
@@ -0,0 +1,113 @@
+﻿using LeetCode.Data;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.UnitTests
+{
+    [TestClass]
+    [TestCategory("0092. Reverse Linked List II")]
+    public class ReverseLinkedListIIFacts
+    {
+        [TestMethod]
+        public void Example1()
+        {
+            var sol = new ReverseLinkedListII();
+            var list = new ListNode(
+                1,
+                new ListNode(2,
+                new ListNode(3,
+                new ListNode(4,
+                new ListNode(5)))));
+
+            var reversed = sol.ReverseBetween(list, 2, 4);
+
+            Assert.AreEqual("1, 4, 3, 2, 5", reversed.ToString());
+        }
+
+        [TestMethod]
+        public void Example2()
+        {
+            var sol = new ReverseLinkedListII();
+            var list = new ListNode(
+                1,
+                new ListNode(2,
+                new ListNode(3,
+                new ListNode(4,
+                new ListNode(5)))));
+
+            var reversed = sol.ReverseBetween(list, 6, 7);
+
+            Assert.AreEqual("1, 2, 3, 4, 5", reversed.ToString());
+        }
+
+        [TestMethod]
+        public void Example3()
+        {
+            var sol = new ReverseLinkedListII();
+            var list = new ListNode(
+                1,
+                new ListNode(2,
+                new ListNode(3)));
+
+            var reversed = sol.ReverseBetween(list, 1, 3);
+
+            Assert.AreEqual("3, 2, 1", reversed.ToString());
+        }
+
+        [TestMethod]
+        public void Example4()
+        {
+            var sol = new ReverseLinkedListII();
+            var list = new ListNode(
+                1,
+                new ListNode(2,
+                new ListNode(3)));
+
+            var reversed = sol.ReverseBetween(list, 1, 2);
+
+            Assert.AreEqual("2, 1, 3", reversed.ToString());
+        }
+
+        [TestMethod]
+        public void Example5()
+        {
+            var sol = new ReverseLinkedListII();
+            var list = new ListNode(
+                1,
+                new ListNode(2,
+                new ListNode(3)));
+
+            var reversed = sol.ReverseBetween(list, 2, 3);
+
+            Assert.AreEqual("1, 3, 2", reversed.ToString());
+        }
+
+        [TestMethod]
+        public void Example6()
+        {
+            var sol = new ReverseLinkedListII();
+            var list = new ListNode(
+                1,
+                new ListNode(2,
+                new ListNode(3)));
+
+            var reversed = sol.ReverseBetween(list, 2, 4);
+
+            Assert.AreEqual("1, 3, 2", reversed.ToString());
+        }
+
+        [TestMethod]
+        public void Example7()
+        {
+            var sol = new ReverseLinkedListII();
+            var list = new ListNode(
+                1,
+                new ListNode(2,
+                new ListNode(3,
+                new ListNode(4))));
+
+            var reversed = sol.ReverseBetween(list, 2, 4);
+
+            Assert.AreEqual("1, 4, 3, 2", reversed.ToString());
+        }
+    }
+}
---
+++ src/LeetCode/0092-ReverseLinkedListII.cs
@@ -0,0 +1,65 @@
+﻿using LeetCode.Data;
+
+namespace LeetCode
+{
+    public class ReverseLinkedListII
+    {
+        public ListNode ReverseBetween(ListNode head, int m, int n)
+        {
+            if (head == null || m < 1 || n < 1 || m >= n)
+            {
+                return head;
+            }
+
+            var index = 1;
+
+            var node = head;
+            var tail = head;
+
+            while (node.next != null)
+            {
+                if (index < m)
+                {
+                    tail = node;
+                    node = node.next;
+                    index++;
+
+                    continue;
+                }
+
+                var reversed = Reverse(node, n - m, out var _);
+
+                if (node == head)
+                {
+                    return reversed;
+                }
+                else
+                {
+                    tail.next = reversed;
+
+                    return head;
+                }
+            }
+
+            return head;
+        }
+
+        private ListNode Reverse(ListNode node, int count, out ListNode tail)
+        {
+            if (count == 0 || node.next == null)
+            {
+                tail = node;
+
+                return node;
+            }
+
+            var reversed = Reverse(node.next, count - 1, out tail);
+
+            node.next = tail.next;
+            tail.next = node;
+            tail = node;
+
+            return reversed;
+        }
+    }
+}
