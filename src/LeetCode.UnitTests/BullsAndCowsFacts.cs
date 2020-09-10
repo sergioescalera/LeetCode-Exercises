d3c7ba41983f0afee136930057e320eb6e3bd90f
Sergio Escalera <sergio.escalera.c@gmail.com>
2020-09-10 14:07:02 -0400

    299. Bulls and Cows

---
+++ src/LeetCode.UnitTests/BullsAndCowsFacts.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.UnitTests
+{
+    [TestCategory("299. Bulls and Cows")]
+    [TestClass]
+    public class BullsAndCowsFacts
+    {
+        [TestMethod]
+        public void Example1()
+        {
+            var sol = new BullsAndCows();
+
+            Assert.AreEqual("1A3B", sol.GetHint("1807", "7810"));
+        }
+
+        [TestMethod]
+        public void Example2()
+        {
+            var sol = new BullsAndCows();
+
+            Assert.AreEqual("1A1B", sol.GetHint("1123", "0111"));
+        }
+
+        [TestMethod]
+        public void Example3()
+        {
+            var sol = new BullsAndCows();
+
+            Assert.AreEqual("1A0B", sol.GetHint("1234", "1111"));
+
+            Assert.AreEqual("1A0B", sol.GetHint("4321", "2222"));
+        }
+    }
+}
---
+++ src/LeetCode/0299-BullsAndCows.cs
@@ -0,0 +1,52 @@
+﻿using System.Linq;
+
+namespace LeetCode
+{
+    public class BullsAndCows
+    {
+        public string GetHint(string secret, string guess)
+        {
+            if (secret == null || guess == null)
+            {
+                return null;
+            }
+
+            if (secret.Length != guess.Length)
+            {
+                return null;
+            }
+
+            var bulls = 0;
+            var cows = 0;
+            var n = secret.Length;
+
+            var letters = secret
+                .GroupBy(c => c)
+                .ToDictionary(c => c.Key, c => c.Count());
+
+            for (int i = 0; i < n; i++)
+            {
+                var c = guess[i];
+
+                if (c == secret[i])
+                {
+                    bulls++;
+                    letters[c]--;
+                }
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                var c = guess[i];
+
+                if (c != secret[i] && letters.ContainsKey(c) && letters[c] > 0)
+                {
+                    cows++;
+                    letters[c]--;
+                }
+            }
+
+            return $"{bulls}A{cows}B";
+        }
+    }
+}
