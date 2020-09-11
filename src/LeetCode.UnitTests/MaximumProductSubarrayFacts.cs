38d934513f785d087949ced76ab9bbf7981158a4
Sergio Escalera <sergio.escalera.c@gmail.com>
2020-09-11 13:53:52 -0400

    152. Maximum Product Subarray

---
+++ src/LeetCode.UnitTests/MaximumProductSubarrayFacts.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.UnitTests
+{
+    [TestCategory("0152. Maximum Product Subarray")]
+    [TestClass]
+    public class MaximumProductSubarrayFacts
+    {
+        [TestMethod]
+        public void ShouldReturn0WhenNullOrEmpty()
+        {
+            var sol = new MaximumProductSubarray();
+
+            Assert.AreEqual(0, sol.MaxProduct(null));
+            Assert.AreEqual(0, sol.MaxProduct(new int[0]));
+        }
+
+        [TestMethod]
+        public void ShouldReturnSingleValue()
+        {
+            var sol = new MaximumProductSubarray();
+
+            Assert.AreEqual(99, sol.MaxProduct(new[] { 99 }));
+            Assert.AreEqual(-99, sol.MaxProduct(new int[] { -99 }));
+        }
+
+        [TestMethod]
+        public void Example1()
+        {
+            var sol = new MaximumProductSubarray();
+
+            Assert.AreEqual(6, sol.MaxProduct(new[] { 2, 3, -2, 4 }));
+        }
+
+        [TestMethod]
+        public void Example2()
+        {
+            var sol = new MaximumProductSubarray();
+
+            Assert.AreEqual(0, sol.MaxProduct(new[] { -2, 0, -1 }));
+        }
+
+        [TestMethod]
+        public void Example3()
+        {
+            var sol = new MaximumProductSubarray();
+
+            Assert.AreEqual(0, sol.MaxProduct(new[] { 0 ,0, -2, 0 }));
+        }
+
+        [TestMethod]
+        public void Example4()
+        {
+            var sol = new MaximumProductSubarray();
+
+            Assert.AreEqual(6, sol.MaxProduct(new[] { -1, -2, -3 }));
+        }
+    }
+}
---
+++ src/LeetCode/0152-MaximumProductSubarray.cs
@@ -0,0 +1,91 @@
+﻿using System;
+
+namespace LeetCode
+{
+    public class MaximumProductSubarray
+    {
+        public int MaxProduct(int[] nums)
+        {
+            if (nums == null || nums.Length == 0)
+            {
+                return 0;
+            }
+
+            if (nums.Length <= 1)
+            {
+                return nums[0];
+            }
+
+            var product = 1;
+            var max = int.MinValue;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                var n = nums[i];
+
+                product = product *= n;
+
+                max = Math.Max(product, max);
+
+                if (n == 0)
+                {
+                    product = 1;
+                }
+            }
+
+            product = 1;
+
+            for (int i = nums.Length - 1; i >= 0; i--)
+            {
+                var n = nums[i];
+
+                product = product *= n;
+
+                max = Math.Max(product, max);
+
+                if (n == 0)
+                {
+                    product = 1;
+                }
+            }
+
+            return max;
+        }
+
+        public int MaxProductNxN(int[] nums)
+        {
+
+            if (nums == null || nums.Length == 0)
+            {
+                return 0;
+            }
+
+            var maxProduct = int.MinValue;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                var max = nums[i];
+                var value = nums[i];
+
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    value *= nums[j];
+
+                    if (value > max)
+                    {
+                        max = value;
+                    }
+
+                    Console.WriteLine($"{i}...{j}");
+                }
+
+                if (max > maxProduct)
+                {
+                    maxProduct = max;
+                }
+            }
+
+            return maxProduct;
+        }
+    }
+}
