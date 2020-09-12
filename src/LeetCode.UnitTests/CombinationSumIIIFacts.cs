c4ca98c2d760bd5ef292660994808624f3d3219c
Sergio Escalera <sergio.escalera.c@gmail.com>
2020-09-12 11:57:11 -0400

    216. Combination Sum III

---
+++ src/LeetCode.UnitTests/CombinationSumIIIFacts.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+
+namespace LeetCode.UnitTests
+{
+    [TestClass]
+    [TestCategory("0216. Combination Sum III")]
+    public class CombinationSumIIIFacts
+    {
+        [TestMethod]
+        public void Example1()
+        {
+            var sol = new CombinationSumIII();
+
+            var expected = new[]
+            {
+                "1, 2, 4"
+            };
+
+            var output = sol
+                .CombinationSum3(k: 3, n: 7)
+                .Select(o => string.Join(", ", o))
+                .ToList();
+
+            CollectionAssert.AreEquivalent(expected, output);
+        }
+
+        [TestMethod]
+        public void Example2()
+        {
+            var sol = new CombinationSumIII();
+
+            var expected = new[]
+            {
+                "1, 2, 6",
+                "1, 3, 5",
+                "2, 3, 4"
+            };
+
+            var output = sol
+                .CombinationSum3(k: 3, n: 9)
+                .Select(o => string.Join(", ", o))
+                .ToList(); ;
+
+            CollectionAssert.AreEquivalent(expected, output);
+        }
+    }
+}
---
+++ src/LeetCode/0216-CombinationSumIII.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode
+{
+    public class CombinationSumIII
+    {
+        public IList<IList<int>> CombinationSum3(int k, int n)
+        {
+            if (n <= 0 || k <= 0)
+            {
+                return new IList<int>[0];
+            }
+
+            var max = 9;
+            var min = 1;
+
+            var combinations = new List<IList<int>>();
+
+            CombinationSum(
+                k, 
+                n,
+                combinations,
+                new List<int>(k),
+                min,
+                max);
+
+            return combinations;
+        }
+
+        private void CombinationSum(
+            int k, 
+            int n,
+            List<IList<int>> combinations,
+            IEnumerable<int> numbers,
+            int number,
+            int max)
+        {
+            if (number > max || k <= 0 || number > n)
+            {
+                return;
+            }
+
+            if (k == 1)
+            {
+                if (number == n)
+                {
+                    combinations
+                        .Add(numbers.Union(new[] { number })
+                        .ToArray());
+                }
+
+                if (number >= n)
+                {
+                    return;
+                }
+
+                CombinationSum(k, n, combinations, numbers, number + 1, max);
+            }
+            else
+            {
+                CombinationSum(k, n, combinations, numbers, number + 1, max);
+
+                if (number + 1 <= n - number)
+                {
+                    CombinationSum(
+                        k - 1,
+                        n - number,
+                        combinations,
+                        numbers.Union(new[] { number }),
+                        number + 1,
+                        max);
+                }
+            }
+        }
+    }
+}
