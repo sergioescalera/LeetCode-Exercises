9fc532ab45301cdfd0446191e6e8e8c1af0aeef5
Sergio Escalera <sergio.escalera.c@gmail.com>
2020-09-13 11:24:50 -0400

    57. Insert Interval

---
+++ src/LeetCode.UnitTests/InsertIntervalFacts.cs
@@ -0,0 +1,181 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.UnitTests
+{
+    [TestClass]
+    [TestCategory("0057. Insert Interval")]
+    public class InsertIntervalFacts
+    {
+        [TestMethod]
+        public void InsertBetween1()
+        {
+            var sol = new InsertInterval();
+
+            var output = sol.Insert(new[]
+            {
+                new[] { 2, 3 },
+                new[] { 6, 9 }
+            },
+            new[] { 0, 1 });
+
+            var expected = new[]
+            {
+                new [] { 0, 1 },
+                new [] { 2, 3 },
+                new [] { 6, 9 }
+            };
+
+            Assert.AreEqual(expected.Length, output.Length);
+
+            for (int i = 0; i < output.Length; i++)
+            {
+                CollectionAssert.AreEqual(expected[i], output[i]);
+            }
+        }
+
+        [TestMethod]
+        public void InsertBetween2()
+        {
+            var sol = new InsertInterval();
+
+            var output = sol.Insert(new[]
+            {
+                new[] { 2, 3 },
+                new[] { 6, 9 }
+            },
+            new[] { 4, 5 });
+
+            var expected = new[]
+            {
+                new [] { 2, 3 },
+                new [] { 4, 5 },
+                new [] { 6, 9 }
+            };
+
+            Assert.AreEqual(expected.Length, output.Length);
+
+            for (int i = 0; i < output.Length; i++)
+            {
+                CollectionAssert.AreEqual(expected[i], output[i]);
+            }
+        }
+
+        [TestMethod]
+        public void InsertBetween3()
+        {
+            var sol = new InsertInterval();
+
+            var output = sol.Insert(new[]
+            {
+                new[] { 2, 3 },
+                new[] { 6, 9 }
+            },
+            new[] { 10, 11 });
+
+            var expected = new[]
+            {
+                new [] { 2, 3 },
+                new [] { 6, 9 },
+                new [] { 10, 11 },
+            };
+
+            Assert.AreEqual(expected.Length, output.Length);
+
+            for (int i = 0; i < output.Length; i++)
+            {
+                CollectionAssert.AreEqual(expected[i], output[i]);
+            }
+        }
+
+        [TestMethod]
+        public void Example1()
+        {
+            var sol = new InsertInterval();
+
+            var output = sol.Insert(new[]
+            {
+                new[] { 1, 3 },
+                new[] { 6, 9 }
+            },
+            new[] { 2, 5 });
+
+            var expected = new[]
+            {
+                new [] { 1, 5 },
+                new [] { 6, 9 }
+            };
+
+            Assert.AreEqual(expected.Length, output.Length);
+
+            for (int i = 0; i < output.Length; i++)
+            {
+                CollectionAssert.AreEqual(expected[i], output[i]);
+            }
+        }
+
+        [TestMethod]
+        public void Example2()
+        {
+            var sol = new InsertInterval();
+
+            var output = sol.Insert(new[]
+            {
+                new[] { 1, 2 },
+                new[] { 3, 5 },
+                new[] { 6, 7 },
+                new[] { 8, 10 },
+                new[] { 12, 16 }
+            },
+            new[] { 4, 8 });
+
+            var expected = new[]
+            {
+                new [] { 1, 2 },
+                new [] { 3, 10 },
+                new [] { 12, 16 }
+            };
+
+            Assert.AreEqual(expected.Length, output.Length);
+
+            for (int i = 0; i < output.Length; i++)
+            {
+                CollectionAssert.AreEqual(expected[i], output[i]);
+            }
+        }
+
+        [TestMethod]
+        public void Example3()
+        {
+            var sol = new InsertInterval();
+
+            var output = sol.Insert(new[]
+            {
+                new[] { 1, 2 },
+                new[] { 3, 5 },
+                new[] { 6, 7 },
+                new[] { 8, 10 },
+                new[] { 12, 16 }
+            },
+            new[] { 0, 11 });
+
+            var expected = new[]
+            {
+                new [] { 0, 11 },
+                new [] { 12, 16 }
+            };
+
+            Assert.AreEqual(expected.Length, output.Length);
+
+            for (int i = 0; i < output.Length; i++)
+            {
+                CollectionAssert.AreEqual(expected[i], output[i]);
+            }
+        }
+    }
+}
---
+++ src/LeetCode/0057-InsertInterval.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace LeetCode
+{
+    public class InsertInterval
+    {
+        public int[][] Insert(int[][] intervals, int[] newInterval)
+        {
+            if (intervals == null)
+            {
+                throw new ArgumentNullException(nameof(intervals));
+            }
+
+            if (newInterval == null)
+            {
+                throw new ArgumentNullException(nameof(newInterval));
+            }
+
+            var x = newInterval[0];
+            var y = newInterval[1];
+
+            var list = new List<int[]>();
+
+            for (int i = 0; i < intervals.Length; )
+            {
+                var interval = intervals[i];
+                
+                if (newInterval == null)
+                {
+                    list.Add(interval);
+                }
+                else if (interval[1] < x)
+                {
+                    list.Add(interval);
+                }
+                else if (y < interval[0])
+                {
+                    list.Add(newInterval);
+                    list.Add(interval);
+                    newInterval = null;
+                }
+                else if (interval[0] <= x && y <= interval[1])
+                {
+                    list.Add(interval);
+                    newInterval = null;
+                }
+                else if (x < interval[0] && y <= interval[1])
+                {
+                    list.Add(new[] { x, interval[1] });
+                    newInterval = null;
+                }
+                else if (interval[0] <= x && interval[1] < y)
+                {
+                    var found = false;
+
+                    for (int j = i + 1; j < intervals.Length; j++)
+                    {
+                        var next = intervals[j];
+
+                        if (y < next[0])
+                        {
+                            found = true;
+
+                            list.Add(new[] { interval[0], y });
+                            newInterval = null;
+
+                            i = j;
+
+                            break;
+                        }
+                        
+                        if (y <= next[1])
+                        {
+                            found = true;
+
+                            list.Add(new[] { interval[0], next[1] });
+                            newInterval = null;
+
+                            i = j + 1;
+
+                            break;
+                        }
+                    }
+
+                    if (found == false)
+                    {
+                        list.Add(new[] { interval[0], y });
+                        newInterval = null;
+                        i = intervals.Length;
+                    }
+
+                    continue;
+                }
+
+                i++;
+            }
+
+            if (newInterval != null)
+            {
+                list.Add(newInterval);
+            }
+
+            return list.ToArray();
+        }
+    }
+}
