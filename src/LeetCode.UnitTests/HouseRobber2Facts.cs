b78a7fe49a5e500c8f7ea6a6c6919f4962c86b07
Sergio Escalera <sergio.escalera.c@gmail.com>
2020-09-14 20:13:20 -0400

    213. House Robber

--- src/LeetCode.UnitTests/HouseRobber2Facts.cs
+++ src/LeetCode.UnitTests/HouseRobber2Facts.cs
@@ -3,13 +3,13 @@
 namespace LeetCode.UnitTests
 {
     [TestClass]
-    [TestCategory("0198. House Robber")]
-    public class HouseRobberFacts
+    [TestCategory("0213. House Robber")]
+    public class HouseRobber2Facts
     {
         [TestMethod]
         public void Empty()
         {
-            var sol = new HouseRobber();
+            var sol = new HouseRobber2();
 
             Assert.AreEqual(0, sol.Rob(null));
             Assert.AreEqual(0, sol.Rob(new int[0]));
@@ -18,7 +18,7 @@ namespace LeetCode.UnitTests
         [TestMethod]
         public void OneHouse()
         {
-            var sol = new HouseRobber();
+            var sol = new HouseRobber2();
 
             Assert.AreEqual(1, sol.Rob(new int[] { 1 }));
             Assert.AreEqual(10, sol.Rob(new int[] { 10 }));
@@ -27,7 +27,7 @@ namespace LeetCode.UnitTests
         [TestMethod]
         public void TwoHouses()
         {
-            var sol = new HouseRobber();
+            var sol = new HouseRobber2();
 
             Assert.AreEqual(2, sol.Rob(new int[] { 1,2 }));
             Assert.AreEqual(10, sol.Rob(new int[] { 10, 9 }));
@@ -36,20 +36,20 @@ namespace LeetCode.UnitTests
         [TestMethod]
         public void ThreeHouses()
         {
-            var sol = new HouseRobber();
+            var sol = new HouseRobber2();
 
-            Assert.AreEqual(4, sol.Rob(new int[] { 1, 3, 3 }));
+            Assert.AreEqual(5, sol.Rob(new int[] { 1, 3, 5 }));
             Assert.AreEqual(21, sol.Rob(new int[] { 10, 21, 10 }));
         }
 
         [TestMethod]
         public void FourHouses()
         {
-            var sol = new HouseRobber();
+            var sol = new HouseRobber2();
 
             Assert.AreEqual(24, sol.Rob(new int[] { 1, 3, 3, 21 }));
             Assert.AreEqual(23, sol.Rob(new int[] { 10, 21, 13, 1 }));
-            Assert.AreEqual(104, sol.Rob(new int[] { 5, 1, 98, 99 }));
+            Assert.AreEqual(103, sol.Rob(new int[] { 5, 1, 98, 99 }));
         }
     }
 }
---
+++ src/LeetCode/0213-HouseRobber.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Linq;
+
+namespace LeetCode
+{
+    public class HouseRobber2
+    {
+        public int Rob(int[] nums)
+        {
+
+            if (nums == null || nums.Length == 0)
+            {
+                return 0;
+            }
+
+            if (nums.Length <= 2)
+            {
+                return nums.Max();
+            }
+
+            var totals = new int[nums.Length];
+
+            Rob(nums, 0, nums.Length - 2, totals);
+
+            Rob(nums, 1, nums.Length - 1, totals);
+
+            return Math.Max(totals[0], totals[1]);
+        }
+
+        public void Rob(int[] nums, int index, int end, int[] totals)
+        {
+            if (index == end)
+            {
+                totals[index] = nums[index];
+            }
+
+            else if (index == end - 1)
+            {
+                totals[index + 1] = nums[index + 1];
+                totals[index] = Math.Max(nums[index], nums[index + 1]);
+            }
+
+            else
+            {
+                Rob(nums, index + 1, end, totals);
+                totals[index] = Math.Max(totals[index + 1], totals[index + 2] + nums[index]);
+            }
+        }
+    }
+}
