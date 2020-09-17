eb7afa2f4770ddda736cdc53f486da0d05e5e1bf
Sergio Escalera <sergio.escalera.c@gmail.com>
2020-09-17 13:03:28 -0400

    1041. Robot Bounded In Circle

---
+++ src/LeetCode.UnitTests/RobotBoundedInCircleFacts.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.UnitTests
+{
+    [TestClass]
+    [TestCategory("1041. Robot Bounded In Circle")]
+    public class RobotBoundedInCircleFacts
+    {
+        [TestMethod]
+        public void Example1()
+        {
+            Assert.IsTrue(new RobotBoundedInCircle().IsRobotBounded("GGLLGG"));
+        }
+
+        [TestMethod]
+        public void Example2()
+        {
+            Assert.IsFalse(new RobotBoundedInCircle().IsRobotBounded("GG"));
+        }
+
+        [TestMethod]
+        public void Example3()
+        {
+            Assert.IsTrue(new RobotBoundedInCircle().IsRobotBounded("GL"));
+        }
+    }
+}
---
+++ src/LeetCode/1041-RobotBoundedInCircle.cs
@@ -0,0 +1,71 @@
+﻿namespace LeetCode
+{
+    public class RobotBoundedInCircle
+    {
+        public bool IsRobotBounded(string instructions)
+        {
+            if (string.IsNullOrEmpty(instructions))
+            {
+                return true;
+            }
+
+            var go = 'G';
+            var left = 'L';
+            var right = 'R';
+
+            var directions = new[]
+            {
+                new
+                {
+                    x = 0,
+                    y = 1
+                },
+                new
+                {
+                    x = 1,
+                    y = 0
+                },
+                new
+                {
+                    x = 0,
+                    y = -1
+                },
+                new
+                {
+                    x = -1,
+                    y = 0
+                }
+            };
+
+            var x = 0;
+            var y = 0;
+
+            var d = 0;
+
+            foreach (var i in instructions)
+            {
+                if (i == go)
+                {
+                    x += directions[d].x;
+                    y += directions[d].y;
+                }
+                if (i == left)
+                {
+                    d = d == 0 ? directions.Length - 1 : d - 1;
+                }
+                if (i == right)
+                {
+                    d++;
+                    d %= directions.Length;
+                }
+            }
+
+            if (x == 0 && y == 0)
+            {
+                return true;
+            }
+
+            return d != 0;
+        }
+    }
+}
