6b73f9d05ee66051f9ef967cca445faeee72efeb
Sergio Escalera <sergio.escalera.c@gmail.com>
2020-09-19 21:22:21 -0400

    1291. Sequential Digits

---
+++ src/LeetCode.UnitTests/SequentialDigitsFacts.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+
+namespace LeetCode.UnitTests
+{
+    [TestClass]
+    [TestCategory("1291. Sequential Digits")]
+    public class SequentialDigitsFacts
+    {
+        [TestMethod]
+        public void Example1()
+        {
+            CollectionAssert.AreEquivalent(
+                new[] { 123, 234 },
+                new SequentialDigitsSolution().SequentialDigits(100, 300).ToArray());
+        }
+
+        [TestMethod]
+        public void Example2()
+        {
+            CollectionAssert.AreEquivalent(
+                new[] { 1234, 2345, 3456, 4567, 5678, 6789, 12345 },
+                new SequentialDigitsSolution().SequentialDigits(1000, 13000).ToArray());
+        }
+    }
+}
---
+++ src/LeetCode/1291-SequentialDigits.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    public class SequentialDigitsSolution
+    {
+        public IList<int> SequentialDigits(int low, int high)
+        {
+            int minDigits = (int)Math.Floor(Math.Log10(low)) + 1;
+            int maxDigits = (int)Math.Floor(Math.Log10(high)) + 1;
+
+            var list = new List<int>();
+            
+            for (int digitCount = minDigits; digitCount <= maxDigits; digitCount++)
+            {
+                int pow10 = (int)Math.Pow(10, digitCount - 1);
+
+                int firstDigit = minDigits == digitCount ?
+                    low / pow10 :
+                    1;
+
+                for (int digit = firstDigit; digit <= 9; digit++)
+                {
+                    if (digit * pow10 > high)
+                    {
+                        break;
+                    }
+
+                    foreach (var seq in SequentialDigits(maxDigits, digitCount - 1, digit))
+                    {
+                        var n = seq + digit * pow10;
+
+                        if (low <= n && n <= high)
+                        {
+                            list.Add(n);
+                        }
+                        else if (n > high)
+                        {
+                            break;
+                        }
+                    }
+                }
+            }
+
+            return list;
+        }
+
+        private IEnumerable<int> SequentialDigits(
+            int digits,
+            int index,
+            int prev)
+        {
+            if (index == 1)
+            {
+                if (prev < 9)
+                {
+                    yield return prev + 1;
+                }
+
+            }
+            else
+            {
+                if (prev < 9)
+                {
+                    var digit = prev + 1;
+
+                    foreach (var seq in SequentialDigits(digits, index - 1, digit))
+                    {
+                        int pow10 = (int)Math.Pow(10, index - 1);
+
+                        var n = seq + digit * pow10;
+
+                        yield return n;
+                    }
+                }
+            }
+
+            yield break;
+        }
+    }
+}
