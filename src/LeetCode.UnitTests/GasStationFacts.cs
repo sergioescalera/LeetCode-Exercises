bb369cfa02b18f9b1970a931748ffbeb5188cd52
Sergio Escalera <sergio.escalera.c@gmail.com>
2020-09-23 19:56:00 -0400

    134. Gas Station

---
+++ src/LeetCode.UnitTests/GasStationFacts.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.UnitTests
+{
+    [TestClass]
+    [TestCategory("0134. Gas Station")]
+    public class GasStationFacts
+    {
+        [TestMethod]
+        public void Empty()
+        {
+            Assert.AreEqual(-1, new GasStation().CanCompleteCircuit(
+                new int[] { },
+                new int[] { }));
+        }
+
+        [TestMethod]
+        public void OneStation()
+        {
+            Assert.AreEqual(-1, new GasStation().CanCompleteCircuit(
+                new int[] { 10 },
+                new int[] { 22 }));
+
+            Assert.AreEqual(0, new GasStation().CanCompleteCircuit(
+                new int[] { 3 },
+                new int[] { 3 }));
+
+            Assert.AreEqual(0, new GasStation().CanCompleteCircuit(
+                new int[] { 3 },
+                new int[] { 2 }));
+        }
+
+        [TestMethod]
+        public void Example1()
+        {
+            Assert.AreEqual(3, new GasStation().CanCompleteCircuit(
+                new[] { 1, 2, 3, 4, 5 },
+                new[] { 3, 4, 5, 1, 2 }));
+        }
+
+        [TestMethod]
+        public void Example2()
+        {
+            Assert.AreEqual(-1, new GasStation().CanCompleteCircuit(
+                new[] { 2, 3, 4 },
+                new[] { 3, 4, 3 }));
+        }
+    }
+}
---
+++ src/LeetCode/0134-GasStation.cs
@@ -0,0 +1,64 @@
+﻿using System.Linq;
+
+namespace LeetCode
+{
+    public class GasStation
+    {
+        public int CanCompleteCircuit(int[] gas, int[] cost)
+        {
+
+            if (gas == null || cost == null)
+            {
+                return -1;
+            }
+
+            if (gas.Length != cost.Length)
+            {
+                return -1;
+            }
+
+            int n = gas.Length;
+
+            int sumGas = gas.Sum();
+            int sumCost = cost.Sum();
+
+            if (sumGas < sumCost)
+            {
+                return -1;
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                if (gas[i] >= cost[i] && CanCompleteCircuit(gas, cost, n, i))
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+
+        private bool CanCompleteCircuit(
+            int[] gas, 
+            int[] cost, 
+            int n,
+            int start)
+        {
+            int tank = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                int j = (start + i) % n;
+
+                tank += gas[j] - cost[j];
+
+                if (tank < 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
