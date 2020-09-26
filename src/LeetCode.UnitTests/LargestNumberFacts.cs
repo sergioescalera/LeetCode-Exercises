a37edaaa0d725974d7df81d7c84267b003535d45
Sergio Escalera <sergio.escalera.c@gmail.com>
2020-09-25 20:52:08 -0400

    0179. Largest Number

---
+++ src/LeetCode.UnitTests/LargestNumberFacts.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.UnitTests
+{
+    [TestClass]
+    [TestCategory("0179. Largest Number")]
+    public class LargestNumberFacts
+    {
+        [TestMethod]
+        public void Example0()
+        {
+            var sol = new LargestNumberSolution();
+
+            Assert.AreEqual("123", sol.LargestNumber(new[] { 123 }));
+
+            Assert.AreEqual("", sol.LargestNumber(new int[] { }));
+        }
+
+        [TestMethod]
+        public void Example1()
+        {
+            var sol = new LargestNumberSolution();
+
+            Assert.AreEqual("210", sol.LargestNumber(new[] { 10, 2 }));
+        }
+
+        [TestMethod]
+        public void Example2()
+        {
+            var sol = new LargestNumberSolution();
+
+            Assert.AreEqual("9534330", sol.LargestNumber(new[] { 3, 30, 34, 5, 9 }));
+        }
+
+        [TestMethod]
+        public void Example3()
+        {
+            var sol = new LargestNumberSolution();
+
+            Assert.AreEqual(
+                "9995929190954321", 
+                sol.LargestNumber(new[] { 91, 92, 909, 99, 95, 5, 1, 2, 3, 4 }));
+        }
+
+        [TestMethod]
+        public void Example4()
+        {
+            var sol = new LargestNumberSolution();
+
+            Assert.AreEqual(
+                "12121",
+                sol.LargestNumber(new[] { 121, 12 }));
+        }
+
+        [TestMethod]
+        public void Example5()
+        {
+            var sol = new LargestNumberSolution();
+
+            Assert.AreEqual(
+                "0",
+                sol.LargestNumber(new[] { 0, 0 }));
+        }
+    }
+}
---
+++ src/LeetCode/0179-LargestNumber.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode
+{
+    public class LargestNumberSolution
+    {
+        public string LargestNumber(int[] nums)
+        {
+
+            if (nums == null || nums.Length == 0)
+            {
+                return "";
+            }
+
+            var sorted = nums
+                .Select(n => new Digits(n))
+                .OrderByDescending(d => d, new Comparer())
+                .Select(d => d.n);
+
+            var str = string.Join("", sorted).TrimStart('0');
+
+            if (string.IsNullOrEmpty(str))
+            {
+                return "0";
+            }
+
+            return str;
+        }
+
+        class Digits
+        {
+
+            public readonly byte[] digits;
+
+            public readonly int n;
+
+            public Digits(int n)
+            {
+                this.n = n;
+                this.digits = GetDigits(n).ToArray();
+            }
+
+            public Digits(byte[] digits)
+            {
+                this.digits = digits;
+            }
+
+            private IEnumerable<byte> GetDigits(int n)
+            {
+
+                do
+                {
+                    yield return (byte)(n % 10);
+
+                    n /= 10;
+
+                } while (n > 0);
+
+                yield break;
+            }
+
+            public override string ToString()
+            {
+                return string.Join("", digits);
+            }
+        }
+
+        class Comparer : IComparer<Digits>
+        {
+
+            public int Compare(Digits x, Digits y)
+            {
+                int len = Math.Min(x.digits.Length, y.digits.Length);
+
+                for (int i = 1; i <= len; i++)
+                {
+
+                    if (x.digits[x.digits.Length - i] ==
+                        y.digits[y.digits.Length - i])
+                    {
+                        continue;
+                    }
+
+                    return x.digits[x.digits.Length - i] - y.digits[y.digits.Length - i];
+                }
+
+                if (x.digits.Length == y.digits.Length)
+                {
+                    return 0;
+                }
+
+                if (x.digits.Length > y.digits.Length)
+                {
+                    return Compare(new Digits(x.digits.Take(x.digits.Length - len).ToArray()), y);
+                }
+
+                return Compare(x, new Digits(y.digits.Take(y.digits.Length - len).ToArray()));
+            }
+        }
+    }
+}
