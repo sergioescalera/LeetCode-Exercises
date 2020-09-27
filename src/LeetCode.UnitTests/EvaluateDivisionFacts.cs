30a8ecc94b332fba63258acea1e4adfa4d336727
Sergio Escalera <sergio.escalera.c@gmail.com>
2020-09-27 13:21:43 -0400

    0399. EvaluateDivision

---
+++ src/LeetCode.UnitTests/EvaluateDivisionFacts.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Linq;
+
+namespace LeetCode.UnitTests
+{
+    [TestClass]
+    [TestCategory("0399. EvaluateDivision")]
+    public class EvaluateDivisionFacts
+    {
+        [TestMethod]
+        public void Exampe1()
+        {
+            var sol = new EvaluateDivision();
+
+            var result = sol.CalcEquation(
+                new[] { new[] { "a", "b" }, new[] { "b", "c" } },
+                new[] { 2.0, 3.0 },
+                new[] {
+                    new[] { "a", "c" },
+                    new[] { "b", "a" },
+                    new[] { "a", "e" },
+                    new[] { "a", "a" },
+                    new[] { "x", "x" } });
+
+            CollectionAssert.AreEqual(
+                new[] { 6.0, 0.5, -1.0, 1.0, -1.0 },
+                result);
+        }
+
+        [TestMethod]
+        public void Exampe2()
+        {
+            var sol = new EvaluateDivision();
+
+            var result = sol.CalcEquation(
+                new[] { new[] { "a", "b" }, new[] { "b", "c" }, new[] { "bc", "cd" } },
+                new[] { 1.5, 2.5, 5.0 },
+                new[] { 
+                    new[] { "a", "c" }, 
+                    new[] { "c", "b" }, 
+                    new[] { "bc", "cd" }, 
+                    new[] { "cd", "bc" } });
+
+            CollectionAssert.AreEqual(
+                new[] { 3.75000, 0.40000, 5.00000, 0.20000 },
+                result);
+        }
+
+        [TestMethod]
+        public void Exampe3()
+        {
+            var sol = new EvaluateDivision();
+
+            var result = sol.CalcEquation(
+                new[] { new[] { "a", "b" } },
+                new[] { 0.5 },
+                new[] {
+                    new[] { "a", "b" },
+                    new[] { "b", "a" },
+                    new[] { "a", "c" },
+                    new[] { "x", "y" } });
+
+            CollectionAssert.AreEqual(
+                new[] { 0.50000, 2.00000, -1.00000, -1.00000 },
+                result);
+        }
+
+        [TestMethod]
+        public void Example4()
+        {
+            var sol = new EvaluateDivision();
+
+            var result = sol.CalcEquation(
+                new[] { 
+                    new[] { "x1", "x2" }, 
+                    new[] { "x2", "x3" }, 
+                    new[] { "x3", "x4" }, 
+                    new[] { "x4", "x5" } },
+                new[] { 3.0, 4.0, 5.0, 6.0 },
+                new[] {
+                    new[] { "x1", "x5" },
+                    new[] { "x5", "x2" },
+                    new[] { "x2", "x4" },
+                    new[] { "x2", "x2" },
+                    new[] { "x2", "x9" },
+                    new[] { "x9", "x9" }});
+
+            CollectionAssert.AreEqual(
+                new[] { 360.00000, 0.00833, 20.00000, 1.00000, -1.00000, -1.00000 },
+                result.Select(x => Math.Round(x, 5)).ToArray());
+        }
+    }
+}
---
+++ src/LeetCode/0399-EvaluateDivision.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode
+{
+    public class EvaluateDivision
+    {
+        public double[] CalcEquation(
+            IList<IList<string>> equations,
+            double[] values,
+            IList<IList<string>> queries)
+        {
+            if (equations == null)
+            {
+                throw new ArgumentNullException(nameof(equations));
+            }
+
+            if (values == null)
+            {
+                throw new ArgumentNullException(nameof(values));
+            }
+
+            if (queries == null)
+            {
+                throw new ArgumentNullException(nameof(queries));
+            }
+
+            var graph = BuildGraph(equations, values);
+
+            var results = SolveQueries(graph, queries);
+
+            return results;
+        }
+
+        private double[] SolveQueries(
+            IDictionary<string, IList<Tuple<string, double>>> graph,
+            IList<IList<string>> queries)
+        {
+            var results = new double[queries.Count];
+
+            for (int i = 0; i < queries.Count; i++)
+            {
+                var query = queries[i];
+                var x = query[0];
+                var y = query[1];
+
+                results[i] = SolveEquation(graph, x, y);
+            }
+
+            return results;
+        }
+
+        private double SolveEquation(
+            IDictionary<string, IList<Tuple<string, double>>> graph,
+            string x, 
+            string y)
+        {
+            if (graph.ContainsKey(x) == false)
+            {
+                return -1;
+            }
+
+            if (x == y)
+            {
+                return 1;
+            }
+
+            var queue = new Queue<Tuple<string, double>>(graph[x].Where(o => o.Item1 != x));
+            var visited = new HashSet<string>(new[] { x });
+
+            while (queue.Count > 0)
+            {
+                var value = queue.Dequeue();
+
+                if (value.Item1 == y)
+                {
+                    return value.Item2;
+                }
+
+                visited.Add(value.Item1);
+
+                foreach (var item in graph[value.Item1])
+                {
+                    if (visited.Contains(item.Item1))
+                    {
+                        continue;
+                    }
+
+                    queue.Enqueue(Tuple.Create(item.Item1, item.Item2 * value.Item2));
+                }
+            }
+
+            return -1;
+        }
+
+        private IDictionary<string, IList<Tuple<string, double>>> BuildGraph(
+            IList<IList<string>> equations, 
+            double[] values)
+        {
+            if (equations.Count != values.Length)
+            {
+                throw new ArgumentException();
+            }
+
+            var graph = new Dictionary<string, IList<Tuple<string, double>>>();
+
+            for (int i = 0; i < equations.Count; i++)
+            {
+                var equation = equations[i];
+                var value = values[i];
+
+                var x = equation[0];
+                var y = equation[1];
+
+                AddEquationToGraph(graph, x, y, value);
+
+                if (value > 0)
+                {
+                    AddEquationToGraph(graph, y, x, 1 / value);
+                }
+            }
+
+            return graph;
+        }
+
+        private void AddEquationToGraph(
+            IDictionary<string, IList<Tuple<string, double>>> graph,
+            string x, 
+            string y, 
+            double value)
+        {
+            if (graph.ContainsKey(x) == false)
+            {
+                graph.Add(x, new List<Tuple<string, double>>
+                {
+                    Tuple.Create(x, 1.0)
+                });
+            }
+
+            var list = graph[x];
+
+            list.Add(Tuple.Create(y, value));
+        }
+    }
+}
