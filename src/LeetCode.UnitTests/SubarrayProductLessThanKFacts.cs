bb3f66338a1bb6d8853b5234a61de4052068cf76
Sergio Escalera <sergio.escalera.c@gmail.com>
2020-09-28 19:44:21 -0400

    713. Subarray Product Less Than K

---
+++ src/LeetCode.UnitTests/SubarrayProductLessThanKFacts.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.UnitTests
+{
+    [TestClass]
+    [TestCategory("0713. Subarray Product Less Than K")]
+    public class SubarrayProductLessThanKFacts
+    {
+        [TestMethod]
+        public void Example1()
+        {
+            var sol = new SubarrayProductLessThanK();
+            
+            Assert.AreEqual(8, sol.NumSubarrayProductLessThanK(new[] { 10, 5, 2, 6 }, 100));
+        }
+
+        [TestMethod]
+        public void Example2()
+        {
+            var sol = new SubarrayProductLessThanK();
+            
+            Assert.AreEqual(0, sol.NumSubarrayProductLessThanK(new[] { 1, 1, 1 }, 1));
+            Assert.AreEqual(6, sol.NumSubarrayProductLessThanK(new[] { 1, 1, 1 }, 2));
+            Assert.AreEqual(4, sol.NumSubarrayProductLessThanK(new[] { 1, 2, 3 }, 6));
+        }
+
+        [TestMethod]
+        public void Example3()
+        {
+            var sol = new SubarrayProductLessThanK();
+
+            Assert.AreEqual(18, sol.NumSubarrayProductLessThanK(new[] { 10, 9, 10, 4, 3, 8, 3, 3, 6, 2, 10, 10, 9, 3 }, 19));
+        }
+
+        [TestMethod]
+        public void Example4()
+        {
+            var sol = new SubarrayProductLessThanK();
+
+            Assert.AreEqual(31, 
+                sol.NumSubarrayProductLessThanK(new[] { 10, 2, 2, 5, 4, 4, 4, 3, 7, 7 }, 289));
+        }
+
+        [TestMethod]
+        public void Example5()
+        {
+            var sol = new SubarrayProductLessThanK();
+
+            Assert.AreEqual(32,
+                sol.NumSubarrayProductLessThanK(
+                    new[] { 7, 6, 8, 4, 9, 3, 2, 10, 7, 9, 9, 6, 3 }, 236));
+        }
+    }
+}
---
+++ src/LeetCode/0713-SubarrayProductLessThanK.cs
@@ -0,0 +1,71 @@
+﻿namespace LeetCode
+{
+    public class SubarrayProductLessThanK
+    {
+        public int NumSubarrayProductLessThanK(int[] nums, int k)
+        {
+            if (nums == null)
+            {
+                return 0;
+            }
+
+            if (nums.Length == 0)
+            {
+                return 0;
+            }
+
+            if (k == 0)
+            {
+                return 0;
+            }
+
+            if (nums.Length == 1)
+            {
+                return nums[0] < k ? 1 : 0;
+            }
+
+            int n = nums.Length;
+            int i = 0;
+            int j = 1;
+            int product = nums[0];
+            int count = 0;
+
+            while (i < n)
+            {
+                while (j < n && product * nums[j] < k)
+                {
+                    product *= nums[j];
+                    j++;
+                }
+
+                if (product < k)
+                {
+                    count += j - i;
+                }
+
+                product /= nums[i];
+
+                i++;
+
+                if (i == j && i < n)
+                {
+                    product *= nums[i];
+
+                    j++;
+                }    
+                else
+                {
+                    while (j > i + 1
+                        && j < n
+                        && product >= k)
+                    {
+                        product /= nums[j];
+                        j--;
+                    }
+                }
+            }
+
+            return count;
+        }
+    }
+}
