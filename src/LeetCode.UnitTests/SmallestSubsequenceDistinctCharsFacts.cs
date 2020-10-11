5dd75b9ef0bfc7ebfff3be0810f488c4717d6fae
Sergio Escalera <sergio.escalera.c@gmail.com>
2020-10-11 17:00:30 -0400

    1081. Smallest Subsequence of Distinct Characters

---
+++ src/LeetCode.UnitTests/SmallestSubsequenceDistinctCharsFacts.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.UnitTests
+{
+    [TestClass]
+    [TestCategory("1081. Smallest Subsequence of Distinct Characters")]
+    public class SmallestSubsequenceDistinctCharsFacts
+    {
+        [TestMethod]
+        public void Example1()
+        {
+            Assert.AreEqual(
+                "abc",
+                new SmallestSubsequenceDistinctChars().SmallestSubsequence("bcabc"));
+        }
+
+        [TestMethod]
+        public void Example2()
+        {
+            Assert.AreEqual(
+                "acdb", 
+                new SmallestSubsequenceDistinctChars().SmallestSubsequence("cbacdcbc"));
+        }
+    }
+}
---
+++ src/LeetCode/1081-SmallestSubsequenceDistinctChars.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode
+{
+    public class SmallestSubsequenceDistinctChars
+    {
+        public String SmallestSubsequence(String s)
+        {
+            var stack = new Stack<char>();
+
+            int[] last = new int[26];
+            int[] visited = new int[26];
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                last[s[i] - 'a'] = i;
+            }
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                int c = s[i] - 'a';
+
+                if (visited[c] > 0)
+                {
+                    continue;
+                }
+
+                visited[c]++;
+
+                while (stack.Any() && stack.Peek() > s[i] && i < last[stack.Peek() - 'a'])
+                {
+                    visited[stack.Pop() - 'a'] = 0;
+                }
+
+                stack.Push(s[i]);
+            }
+
+            var sb = new StringBuilder();
+
+            foreach (var c in stack)
+            {
+                sb.Insert(0, c);
+            } 
+                
+            return sb.ToString();
+        }
+    }
+}
