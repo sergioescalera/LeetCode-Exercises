72203338a9c3d7c3e341d61b6fa276c43809cb62
Sergio Escalera <sergio.escalera.c@gmail.com>
2020-10-22 11:36:32 -0400

    109. Convert Sorted List to Binary Search Tree

---
+++ src/LeetCode.UnitTests/ConvertSortedListToBinarySearchTreeFacts.cs
@@ -0,0 +1,19 @@
+﻿using LeetCode.Data;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.UnitTests
+{
+    [TestClass]
+    [TestCategory("0109. Convert Sorted List to Binary Search Tree")]
+    public class ConvertSortedListToBinarySearchTreeFacts
+    {
+        [TestMethod]
+        public void Example1()
+        {
+            var sol = new ConvertSortedListToBinarySearchTree();
+            var tree = sol.SortedListToBST(new ListNode("-10 -3 0 5 9"));
+
+            Assert.IsNotNull(tree);
+        }
+    }
+}
---
+++ src/LeetCode/0109-ConvertSortedListToBinarySearchTree.cs
@@ -0,0 +1,47 @@
+﻿using LeetCode.Data;
+
+namespace LeetCode
+{
+    public class ConvertSortedListToBinarySearchTree
+    {
+        public TreeNode SortedListToBST(ListNode head)
+        {
+            return SortedListToBST(head, null);
+        }
+
+        public TreeNode SortedListToBST(ListNode head, ListNode tail)
+        {
+
+            if (head == tail)
+            {
+                return null;
+            }
+
+            var mid = FindMid(head, tail);
+
+            return new TreeNode(
+                mid.val,
+                SortedListToBST(head, mid),
+                SortedListToBST(mid.next, tail));
+        }
+
+        private ListNode FindMid(ListNode start, ListNode end)
+        {
+            if (start == end)
+            {
+                return null;
+            }
+
+            var slow = start;
+            var fast = start.next;
+
+            while (fast != end && fast.next != end)
+            {
+                slow = slow.next;
+                fast = fast.next.next;
+            }
+
+            return slow;
+        }
+    }
+}
