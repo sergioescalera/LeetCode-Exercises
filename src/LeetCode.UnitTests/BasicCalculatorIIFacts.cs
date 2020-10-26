c56b45aa1ff07d6d775ab2d797e9f61ce23c116e
Sergio Escalera <sergio.escalera.c@gmail.com>
2020-10-26 15:28:20 -0400

    227. Basic Calculator II

---
+++ src/LeetCode.UnitTests/BasicCalculatorIIFacts.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.UnitTests
+{
+    [TestClass]
+    [TestCategory("0227. Basic Calculator II")]
+    public class BasicCalculatorIIFacts
+    {
+        [TestMethod]
+        public void Example1()
+        {
+            var sol = new BasicCalculatorII();
+
+            var result = sol.Calculate("3 + 2 * 2");
+
+            Assert.AreEqual(7, result);
+        }
+
+        [TestMethod]
+        public void Example2()
+        {
+            var sol = new BasicCalculatorII();
+
+            var result = sol.Calculate(" 3/2 ");
+
+            Assert.AreEqual(1, result);
+        }
+
+        [TestMethod]
+        public void Example3()
+        {
+            var sol = new BasicCalculatorII();
+
+            var result = sol.Calculate(" 3+5 / 2 ");
+
+            Assert.AreEqual(5, result);
+        }
+    }
+}
---
+++ src/LeetCode/0227-BasicCalculatorII.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode
+{
+    public class BasicCalculatorII
+    {
+        public int Calculate(string s)
+        {
+
+            if (string.IsNullOrEmpty(s))
+            {
+                return 0;
+            }
+
+            var exp = s.Trim().Replace(" ", "");
+
+            var n = exp.Length;
+            var stack = new Stack<int>();
+
+            var currentNumber = 0;
+
+            var op = '+';
+
+            for (var i = 0; i < n; i++)
+            {
+
+                var c = exp[i];
+
+                if (char.IsDigit(c))
+                {
+                    currentNumber = currentNumber * 10 + (c - '0');
+                }
+
+                if (char.IsDigit(c) == false || i == n - 1)
+                {
+                    if (op == '-')
+                    {
+                        stack.Push(-currentNumber);
+                    }
+
+                    else if (op == '+')
+                    {
+                        stack.Push(currentNumber);
+                    }
+
+                    else if (op == '*')
+                    {
+                        stack.Push(stack.Pop() * currentNumber);
+                    }
+
+                    else if (op == '/')
+                    {
+                        stack.Push(stack.Pop() / currentNumber);
+                    }
+
+                    currentNumber = 0;
+                    op = c;
+                }
+            }
+
+            var result = 0;
+
+            while (stack.Count > 0)
+            {
+                result += stack.Pop();
+            }
+
+            return result;
+        }
+    }
+}
