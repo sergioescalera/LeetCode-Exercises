6c8f0e4b77d9ec1212e59b5f52abda2ac3c88f78
Sergio Escalera <sergio.escalera.c@gmail.com>
2020-12-05 15:15:54 -0500

    0332. Reconstruct Itinerary

---
+++ src/LeetCode.UnitTests/ReconstructItineraryFacts.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.UnitTests
+{
+    [TestClass]
+    [TestCategory("0332. Reconstruct Itinerary")]
+    public class ReconstructItineraryFacts
+    {
+        [TestMethod]
+        public void Example1()
+        {
+            var sol = new ReconstructItinerary();
+
+            var output = sol.FindItinerary(new[]
+            {
+                new [] { "MUC", "LHR" },
+                new [] { "JFK", "MUC" },
+                new [] { "SFO", "SJC" },
+                new [] { "LHR", "SFO" }
+            });
+
+            Assert.IsNotNull(output);
+            Assert.AreEqual("JFK,MUC,LHR,SFO,SJC", string.Join(",", output));
+        }
+
+        [TestMethod]
+        public void Example2()
+        {
+            var sol = new ReconstructItinerary();
+
+            var output = sol.FindItinerary(new[]
+            {
+                new [] { "JFK", "SFO" },
+                new [] { "JFK", "ATL" },
+                new [] { "SFO", "ATL" },
+                new [] { "ATL", "JFK" },
+                new [] { "ATL", "SFO" }
+            });
+
+            Assert.IsNotNull(output);
+            Assert.AreEqual("JFK,ATL,JFK,SFO,ATL,SFO", string.Join(",", output));
+        }
+    }
+}
---
+++ src/LeetCode/0332-ReconstructItinerary.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode
+{
+    public class ReconstructItinerary
+    {
+        public IList<string> FindItinerary(IList<IList<string>> tickets)
+        {
+            if (tickets == null || tickets.Count == 0)
+            {
+                return new string[0];
+            }
+
+            var itinerary = new List<string>(new[] { "JFK" });
+
+            FindItinerary(
+                tickets.Count,
+                tickets
+                    .GroupBy(t => t[0])
+                    .ToDictionary(g => g.Key, g => new HashSet<string>(g.OrderBy(t => t[1]).Select(t => t[1]))),
+                "JFK",
+                itinerary,
+                tickets
+                    .Select(t => $"{t[0]}-{t[1]}")
+                    .GroupBy(t => t)
+                    .ToDictionary(g => g.Key, g => g.Count())
+            );
+
+            return itinerary;
+        }
+
+        bool FindItinerary(
+            int total,
+            IDictionary<string, HashSet<string>> tickets,
+            string current,
+            IList<string> itinerary,
+            IDictionary<string, int> visited)
+        {
+            if (itinerary.Count == total + 1)
+            {
+                return true;
+            }
+
+            if (tickets.ContainsKey(current) == false)
+            {
+                return false;
+            }
+
+            var adjacent = tickets[current];
+
+            foreach (var next in adjacent)
+            {
+
+                var ticket = $"{current}-{next}";
+
+                if (visited[ticket] <= 0)
+                {
+                    continue;
+                }
+
+                visited[ticket]--;
+
+                itinerary.Add(next);
+
+                if (FindItinerary(total, tickets, next, itinerary, visited))
+                {
+                    return true;
+                }
+
+                itinerary.RemoveAt(itinerary.Count - 1);
+
+                visited[ticket]++;
+            }
+
+            return false;
+        }
+    }
+}
