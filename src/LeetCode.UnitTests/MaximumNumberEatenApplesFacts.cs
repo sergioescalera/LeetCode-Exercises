aeaea3e81bcb9c096d4476d52b42ffd9f1e6c079
Sergio Escalera <sergio.escalera.c@gmail.com>
2021-03-03 10:23:50 -0500

    1705. Maximum Number of Eaten Apples

---
+++ src/LeetCode.UnitTests/MaximumNumberEatenApplesFacts.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.UnitTests
+{
+    [TestClass]
+    [TestCategory("1705. Maximum Number of Eaten Apples")]
+    public class MaximumNumberEatenApplesFacts
+    {
+        [TestMethod]
+        public void Example1()
+        {
+            var sol = new MaximumNumberEatenApples();
+
+            var actual = sol.EatenApples(new[] { 1, 2, 3, 5, 2 }, new[] { 3, 2, 1, 4, 2 });
+
+            var expected = 7;
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Example2()
+        {
+            var sol = new MaximumNumberEatenApples();
+
+            var actual = sol.EatenApples(new[] { 3, 0, 0, 0, 0, 2 }, new[] { 3, 0, 0, 0, 0, 2 });
+
+            var expected = 5;
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ src/LeetCode/1705-MaximumNumberEatenApples.cs
@@ -0,0 +1,125 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode
+{
+    public class MaximumNumberEatenApples
+    {
+        public int EatenApples(int[] apples, int[] days)
+        {
+
+            if (apples == null || days == null)
+            {
+                return 0;
+            }
+
+            var n = apples.Length;
+            var eaten = 0;
+            var set = new SortedSet<Apples>(new MyComparer());
+            var day = 0;
+
+            for (; day < n; day++)
+            {
+
+                if (set.Count == 0)
+                {
+
+                    if (apples[day] == 0)
+                    {
+                        continue;
+                    }
+
+                    eaten++;
+
+                    StoreApples(set, apples, days, day, eatenThisDay: 1);
+                }
+                else
+                {
+
+                    RemoveExpired(set, day);
+
+                    StoreApples(set, apples, days, day);
+
+                    EatApple(set, ref eaten);
+                }
+            }
+
+            while (set.Count > 0)
+            {
+
+                RemoveExpired(set, day);
+
+                EatApple(set, ref eaten);
+
+                day++;
+            }
+
+            return eaten;
+        }
+
+        private void StoreApples(
+            SortedSet<Apples> set, 
+            int[] apples,
+            int[] days, 
+            int day,
+            int eatenThisDay = 0)
+        {
+            if (apples[day] > eatenThisDay)
+            {
+                set.Add(new Apples
+                {
+                    day = day,
+                    count = apples[day] - eatenThisDay,
+                    expire = day + days[day]
+                });
+            }
+        }
+
+        private void EatApple(SortedSet<Apples> set, ref int eaten)
+        {
+            if (set.Count == 0)
+            {
+                return;
+            }
+
+            eaten++;
+
+            set.Min.count--;
+
+            if (set.Min.count == 0)
+            {
+                set.Remove(set.Min);
+            }
+        }
+
+        private void RemoveExpired(SortedSet<Apples> set, int day)
+        {
+            while (set.Count > 0 && set.Min.expire <= day)
+            {
+                set.Remove(set.Min);
+            }
+        }
+
+        class Apples
+        {
+
+            public int day;
+            public int count;
+            public int expire;
+        }
+
+        class MyComparer : IComparer<Apples>
+        {
+
+            public int Compare(Apples a, Apples b)
+            {
+
+                if (a.day == b.day)
+                {
+                    return 0;
+                }
+
+                return a.expire > b.expire ? 1 : -1;
+            }
+        }
+    }
+}
