33c812ed89b238c0df4aee7ba19bfb37f8f60c31
Sergio Escalera <sergio.escalera.c@gmail.com>
2021-03-25 10:43:58 -0400

    0523. Continuous Subarray Sum

---
+++ src/LeetCode.UnitTests/ContinuousSubarraySumSolutionFacts.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.UnitTests
+{
+    [TestClass]
+    [TestCategory("0523. Continuous Subarray Sum")]
+    public class ContinuousSubarraySumSolutionFacts
+    {
+        [TestMethod]
+        public void Example1()
+        {
+            var sol = new ContinuousSubarraySumSolution();
+
+            var result = sol.CheckSubarraySum(new[] { 23, 2, 4, 6, 7 }, 6);
+
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void Example2()
+        {
+            var sol = new ContinuousSubarraySumSolution();
+
+            var result = sol.CheckSubarraySum(new[] { 23, 2, 6, 4, 7 }, 6);
+
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void Example3()
+        {
+            var sol = new ContinuousSubarraySumSolution();
+
+            var result = sol.CheckSubarraySum(new[] { 1, 2, 3 }, 6);
+
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void Example4()
+        {
+            var sol = new ContinuousSubarraySumSolution();
+
+            var result = sol.CheckSubarraySum(new[] { 1, 2, 3 }, 7);
+
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void Example5()
+        {
+            var sol = new ContinuousSubarraySumSolution();
+
+            var result = sol.CheckSubarraySum(new[] { 1, 2, 3 }, 4);
+
+            Assert.IsFalse(result);
+        }
+    }
+}
---
+++ src/LeetCode/0523-ContinuousSubarraySum.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace LeetCode
+{
+    public class ContinuousSubarraySumSolution
+    {
+        public bool CheckSubarraySum(int[] nums, int k)
+        {
+
+            if (nums == null)
+            {
+                return false;
+            }
+
+            if (nums.Length == 0)
+            {
+                return false;
+            }
+
+            if (nums.Length == 1)
+            {
+                return nums[0] == k;
+            }
+
+            var start = 0;
+            var end = 0;
+            var sum = 0L;
+
+            while (end < nums.Length)
+            {
+
+                sum += nums[end];
+
+                if (sum == k)
+                {
+                    return true;
+                }
+
+                if (sum < k)
+                {
+
+                    end++;
+
+                }
+                else
+                {
+
+                    while (start < end && sum >= k)
+                    {
+
+                        if (sum == k)
+                        {
+                            return true;
+                        }
+
+                        sum -= nums[start];
+
+                        start++;
+                    }
+
+                    if (sum == k)
+                    {
+                        return true;
+                    }
+
+                    if (start == end && sum > k)
+                    {
+                        sum -= nums[start];
+
+                        start++;
+                        end++;
+                    }
+                }
+            }
+
+            return false;
+        }
+    }
+}
