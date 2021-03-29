debf1938ab5389af58e6561892eb27c851c6c259
Sergio Escalera <sergio.escalera.c@gmail.com>
2021-03-29 13:31:02 -0400

    0971. Flip Binary Tree To Match Preorder Traversal

---
+++ src/LeetCode.UnitTests/FlipBinaryTreeToMatchPreorderTraversalFacts.cs
@@ -0,0 +1,54 @@
+﻿using LeetCode.Data;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Linq;
+
+namespace LeetCode.UnitTests
+{
+    [TestClass]
+    [TestCategory("0971. Flip Binary Tree To Match Preorder Traversal")]
+    public class FlipBinaryTreeToMatchPreorderTraversalFacts
+    {
+        [TestMethod]
+        public void Example1()
+        {
+            var sol = new FlipBinaryTreeToMatchPreorderTraversal();
+
+            var result = sol.FlipMatchVoyage(new TreeNode(1, new TreeNode(2)), new[] { 2, 1 });
+
+            CollectionAssert.AreEqual(new[] { -1 }, result.ToArray());
+        }
+
+        [TestMethod]
+        public void Example2()
+        {
+            var sol = new FlipBinaryTreeToMatchPreorderTraversal();
+
+            var result = sol.FlipMatchVoyage(new TreeNode(1, new TreeNode(2), new TreeNode(3)), new[] { 1, 3, 2 });
+
+            CollectionAssert.AreEqual(new[] { 1 }, result.ToArray());
+        }
+
+        [TestMethod]
+        public void Example3()
+        {
+            var sol = new FlipBinaryTreeToMatchPreorderTraversal();
+
+            var result = sol.FlipMatchVoyage(new TreeNode(1, new TreeNode(2), new TreeNode(3)), new[] { 1, 2, 3 });
+
+            CollectionAssert.AreEqual(Array.Empty<int>(), result.ToArray());
+        }
+
+        [TestMethod]
+        public void Example4()
+        {
+            var sol = new FlipBinaryTreeToMatchPreorderTraversal();
+
+            var result = sol.FlipMatchVoyage(
+                new TreeNode(1, new TreeNode(2), new TreeNode(3, new TreeNode(4), new TreeNode(5))),
+                new[] { 1, 3, 5, 4, 2 });
+
+            CollectionAssert.AreEqual(new[] { 1, 3 }, result.ToArray());
+        }
+    }
+}
---
+++ src/LeetCode/0971-FlipBinaryTreeToMatchPreorderTraversal.cs
@@ -0,0 +1,75 @@
+﻿using LeetCode.Data;
+using System;
+using System.Collections.Generic;
+
+namespace LeetCode
+{
+    public class FlipBinaryTreeToMatchPreorderTraversal
+    {
+        public IList<int> FlipMatchVoyage(TreeNode root, int[] voyage)
+        {
+
+            if (root == null)
+            {
+                return Array.Empty<int>();
+            }
+
+            var flip = FlipMatchVoyage(root, voyage, 0);
+
+            if (flip.success)
+            {
+                return flip.list;
+            }
+
+            return new[] { -1 };
+        }
+
+        (IList<int> list, int count, bool success) FlipMatchVoyage(TreeNode node, int[] voyage, int index)
+        {
+
+            if (node == null)
+            {
+                return (Array.Empty<int>(), 0, true);
+            }
+
+            if (index >= voyage.Length || node.val != voyage[index])
+            {
+                return (Array.Empty<int>(), 0, false);
+            }
+
+            var list = new List<int>();
+
+            if (node.left != null &&
+                node.right != null &&
+                index + 1 < voyage.Length &&
+                node.right.val == voyage[index + 1])
+            {
+                var temp = node.right;
+
+                node.right = node.left;
+                node.left = temp;
+
+                list.Add(node.val);
+            }
+
+            var left = FlipMatchVoyage(node.left, voyage, index + 1);
+
+            if (!left.success)
+            {
+                return left;
+            }
+
+            var right = FlipMatchVoyage(node.right, voyage, index + 1 + left.count);
+
+            if (!right.success)
+            {
+                return right;
+            }
+
+            list.AddRange(left.list);
+            list.AddRange(right.list);
+
+            return (list, left.count + right.count + 1, true);
+        }
+    }
+}
