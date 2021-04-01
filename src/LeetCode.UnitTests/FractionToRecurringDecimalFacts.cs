97b821c12ea972170a3eafab5d04ccbd25ab2e1c
Sergio Escalera <sergio.escalera.c@gmail.com>
2021-04-01 10:26:16 -0400

    0166. Fraction to Recurring Decimal

---
+++ src/LeetCode.UnitTests/FractionToRecurringDecimalFacts.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.UnitTests
+{
+    [TestClass]
+    [TestCategory("0166. Fraction to Recurring Decimal")]
+    public class FractionToRecurringDecimalFacts
+    {
+        [TestMethod]
+        public void Example1()
+        {
+            var sol = new FractionToRecurringDecimal();
+
+            Assert.AreEqual("0.5", sol.FractionToDecimal(1, 2));
+
+            Assert.AreEqual("1.5", sol.FractionToDecimal(3, 2));
+
+            Assert.AreEqual("1.5", sol.FractionToDecimal(-3, -2));
+
+            Assert.AreEqual("-1.5", sol.FractionToDecimal(-3, 2));
+
+            Assert.AreEqual("-0.5", sol.FractionToDecimal(1, -2));
+        }
+
+        [TestMethod]
+        public void Example2()
+        {
+            var sol = new FractionToRecurringDecimal();
+
+            Assert.AreEqual("2", sol.FractionToDecimal(2, 1));
+
+            Assert.AreEqual("1", sol.FractionToDecimal(2, 2));
+
+            Assert.AreEqual("-2", sol.FractionToDecimal(-2, 1));
+
+            Assert.AreEqual("-1", sol.FractionToDecimal(2, -2));
+
+            Assert.AreEqual("2", sol.FractionToDecimal(-2, -1));
+
+            Assert.AreEqual("1", sol.FractionToDecimal(-2, -2));
+        }
+
+        [TestMethod]
+        public void Example3()
+        {
+            var sol = new FractionToRecurringDecimal();
+
+            Assert.AreEqual("0.(6)", sol.FractionToDecimal(2, 3));
+        }
+
+        [TestMethod]
+        public void Example4()
+        {
+            var sol = new FractionToRecurringDecimal();
+
+            Assert.AreEqual("0.(012)", sol.FractionToDecimal(4, 333));
+        }
+
+        [TestMethod]
+        public void Example5()
+        {
+            var sol = new FractionToRecurringDecimal();
+
+            Assert.AreEqual("-233.(3231)", sol.FractionToDecimal(777666, -3333));
+        }
+    }
+}
---
+++ src/LeetCode/0166-FractionToRecurringDecimal.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    public class FractionToRecurringDecimal
+    {
+        public string FractionToDecimal(int num, int den)
+        {
+            if (num == 0)
+            {
+                return "0";
+            }
+
+            if (den == 0)
+            {
+                return "0";
+            }
+
+            var sign = (num > 0 && den > 0) || (num < 0 && den < 0) ? 1 : -1;
+
+            var numerator = Math.Abs((long)num);
+            var denominator = Math.Abs((long)den);
+
+            if (denominator == 1)
+            {
+                return (sign * numerator).ToString();
+            }
+
+            var result = new StringBuilder();
+            var dec = false;
+
+            if (numerator < denominator)
+            {
+                result.Append("0.");
+
+                numerator *= 10;
+
+                dec = true;
+            }
+
+            var map = new Dictionary<long, int>();
+
+            while (numerator != 0)
+            {
+                if (dec)
+                {
+                    if (map.ContainsKey(numerator))
+                    {
+                        var index = map[numerator];
+
+                        result.Insert(index, "(");
+                        result.Append(")");
+
+                        break;
+                    }
+                    else
+                    {
+                        map.Add(numerator, result.Length);
+                    }
+                }
+
+                if (numerator < denominator)
+                {
+                    while (numerator < denominator)
+                    {
+                        if (dec)
+                        {
+                            result.Append("0");
+                        }
+                        else
+                        {
+                            result.Append(".");
+
+                            dec = true;
+                        }
+
+                        numerator *= 10;
+                    }
+                }
+                else
+                {
+                    var d = numerator / denominator;
+                    var r = numerator % denominator;
+
+                    result.Append(d);
+
+                    numerator = r;
+
+                    if (dec)
+                    {
+                        numerator *= 10;
+                    }
+                }
+            }
+
+            var str = result.ToString();
+
+            if (sign < 0)
+            {
+                str = $"-{str}";
+            }
+
+            return str;
+        }
+    }
+}
