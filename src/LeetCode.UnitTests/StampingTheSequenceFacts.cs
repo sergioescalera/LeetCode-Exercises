0b761facd297cae4806be74f3ad43b885d04602b
Sergio Escalera <sergio.escalera.c@gmail.com>
2021-04-01 09:48:34 -0400

    0936. Stamping The Sequence - 213 / 262 test cases passed.

---
+++ src/LeetCode.UnitTests/StampingTheSequenceFacts.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.UnitTests
+{
+    [TestClass]
+    [TestCategory("0936. Stamping The Sequence")]
+    public class StampingTheSequenceFacts
+    {
+        [TestMethod]
+        public void Example1()
+        {
+            var sol = new StampingTheSequence();
+
+            var result = sol.MovesToStamp("abc", "ababc");
+
+            Assert.AreEqual("0,2", string.Join(",", result));
+        }
+
+        [TestMethod]
+        public void Example2()
+        {
+            var sol = new StampingTheSequence();
+
+            var result = sol.MovesToStamp("abca", "aabcaca");
+
+            Assert.AreEqual("0,3,1", string.Join(",", result));
+        }
+
+        [TestMethod]
+        public void Example3()
+        {
+            var sol = new StampingTheSequence();
+
+            var result = sol.MovesToStamp("h", "hhhhh");
+
+            Assert.AreEqual("4,3,2,1,0", string.Join(",", result));
+        }
+
+        [TestMethod]
+        public void Example4()
+        {
+            var sol = new StampingTheSequence();
+
+            var result = sol.MovesToStamp("uskh", "uskhkhhskh");
+
+            Assert.AreEqual("6,3,2,0", string.Join(",", result));
+        }
+    }
+}
---
+++ src/LeetCode/0936-StampingTheSequence.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode
+{
+    public class StampingTheSequence
+    {
+        public int[] MovesToStamp(string stamp, string target)
+        {
+
+            if (string.IsNullOrEmpty(stamp) ||
+                string.IsNullOrEmpty(target))
+            {
+                return Array.Empty<int>();
+            }
+
+            var result = new List<int>();
+
+            var replace = new string(Enumerable.Repeat('?', stamp.Length).ToArray());
+
+            var total = 0;
+
+            var index = -1;
+
+            while (total < target.Length)
+            {
+                var count = 0;
+
+                for (int n = stamp.Length; n > 0; n--)
+                {
+                    var temp1 = stamp.Substring(0, n).PadRight(stamp.Length, '?');
+                    var temp2 = stamp.Substring(stamp.Length - n, n).PadLeft(stamp.Length, '?');
+
+                    index = target.IndexOf(temp1);
+
+                    while (index >= 0)
+                    {
+                        target = target.Substring(0, index) + replace + target.Substring(index + replace.Length);
+
+                        result.Add(index);
+
+                        count += n;
+
+                        index = target.IndexOf(temp1);
+                    }
+
+                    index = target.IndexOf(temp2);
+
+                    while (index >= 0)
+                    {
+                        target = target.Substring(0, index) + replace + target.Substring(index + replace.Length);
+
+                        result.Add(index);
+
+                        count += n;
+
+                        index = target.IndexOf(temp2);
+                    }
+                }
+
+                if (count == 0)
+                {
+                    break;
+                }
+
+                total += count;
+            }
+
+            if (total < target.Length)
+            {
+                return Array.Empty<int>();
+            }
+
+            result.Reverse();
+
+            return result.ToArray();
+        }
+    }
+}
