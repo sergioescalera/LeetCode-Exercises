025f751a9e8ebb4b5db0b75ef51d9a05dba7177f
Sergio Escalera <sergio.escalera.c@gmail.com>
2021-04-03 14:39:25 -0400

    LRU Cache

---
+++ src/LeetCode.UnitTests/LRUCacheFacts.cs
@@ -0,0 +1,26 @@
+﻿using LeetCode.Data;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.UnitTests
+{
+    [TestClass]
+    [TestCategory("LRU Cache")]
+    public class LRUCacheFacts
+    {
+        [TestMethod]
+        public void Example1()
+        {
+            var cache = new LRUCache<int, int>(2, -1);
+
+            cache.Put(2, 1);
+            cache.Put(2, 2);
+
+            Assert.AreEqual(2, cache.Get(2));
+
+            cache.Put(1, 1);
+            cache.Put(4, 1);
+
+            Assert.AreEqual(-1, cache.Get(2));
+        }
+    }
+}
---
+++ src/LeetCode/Data/LRUCache.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode.Data
+{
+    public class LRUCache<TKey, TData>
+    {
+        protected readonly Int32 capacity;
+        protected readonly Dictionary<TKey, LRUCacheEntry> map;
+        protected readonly LinkedList<LRUCacheNode> list;
+        protected readonly TData @default;
+
+        public LRUCache(Int32 capacity, TData @default = default(TData))
+        {
+            if (capacity <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(capacity));
+            }
+
+            this.capacity = capacity;
+            this.map = new Dictionary<TKey, LRUCacheEntry>();
+            this.list = new LinkedList<LRUCacheNode>();
+            this.@default = @default;
+        }
+
+        public virtual TData Get(TKey key)
+        {
+            if (map.TryGetValue(key, out var entry) == false)
+            {
+                return @default;
+            }
+
+            MoveToFront(entry);
+
+            return entry.Node.Value.Data;
+        }
+
+        public virtual void Put(TKey key, TData value)
+        {
+            if (map.TryGetValue(key, out var entry))
+            {
+                entry.Node.Value.Data = value;
+
+                MoveToFront(entry);
+            }
+            else
+            {
+                var node = new LinkedListNode<LRUCacheNode>(new LRUCacheNode(key, value));
+                
+                entry = new LRUCacheEntry(node);
+
+                list.AddFirst(node);
+                map.Add(key, entry);
+
+                EnsureCapacity();
+            }
+        }
+
+        public override string ToString()
+        {
+            return string.Join(", ", list.Select(x => $"[{x.Key}, {x.Data}]"));
+        }
+
+        private void EnsureCapacity()
+        {
+            if (list.Count <= capacity)
+            {
+                return;
+            }
+
+            var last = list.Last;
+            var key = last.Value.Key;
+
+            list.Remove(last);
+
+            if (map.ContainsKey(key))
+            {
+                map.Remove(key);
+            }
+        }
+
+        private void MoveToFront(LRUCacheEntry entry)
+        {
+            var node = entry.Node;
+            var key = node.Value.Key;
+
+            list.Remove(node);
+
+            entry.Node = list.AddFirst(node.Value);
+        }
+
+        protected class LRUCacheEntry
+        {
+            public LinkedListNode<LRUCacheNode> Node
+            {
+                get; set;
+            }
+
+            public LRUCacheEntry(LinkedListNode<LRUCacheNode> node)
+            {
+                Node = node;
+            }
+
+            public override string ToString() => Node?.ToString();
+        }
+
+        protected class LRUCacheNode
+        {
+            public TKey Key
+            {
+                get;
+            }
+
+            public TData Data
+            {
+                get; set;
+            }
+
+            public LRUCacheNode(TKey key, TData data)
+            {
+                Key = key;
+                Data = data;
+            }
+
+            public override string ToString()
+            {
+                return $"[{Key}, {Data}]";
+            }
+        }
+    }
+}
