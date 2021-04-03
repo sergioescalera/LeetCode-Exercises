db76589fe285d9aae215af76ddf44c36d8b4563f
Sergio Escalera <sergio.escalera.c@gmail.com>
2021-04-03 11:46:46 -0400

    Quick sort implementation

---
+++ src/LeetCode.UnitTests/QuickSortFacts.cs
@@ -0,0 +1,43 @@
+﻿using LeetCode.Algorithm;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.UnitTests
+{
+    [TestClass]
+    [TestCategory("Quick Sort")]
+    public class QuickSortFacts
+    {
+        [TestMethod]
+        public void SameElement()
+        {
+            var array = new[] { 4, 4, 4 };
+            var expected = new[] { 4, 4, 4 };
+
+            QuickSort.Sort(array);
+
+            CollectionAssert.AreEqual(expected, array);
+        }
+
+        [TestMethod]
+        public void SortedArray()
+        {
+            var array = new[] { 1, 2, 3 };
+            var expected = new[] { 1, 2, 3 };
+
+            QuickSort.Sort(array);
+
+            CollectionAssert.AreEqual(expected, array);
+        }
+
+        [TestMethod]
+        public void ReversedArray()
+        {
+            var array = new[] { 3, 2, 1 };
+            var expected = new[] { 1, 2, 3 };
+
+            QuickSort.Sort(array);
+
+            CollectionAssert.AreEqual(expected, array);
+        }
+    }
+}
---
+++ src/LeetCode/Algorithm/QuickSort.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace LeetCode.Algorithm
+{
+    public class QuickSort
+    {
+        public static void Sort(Int32[] array)
+        {
+            Sort(array, 0, array.Length - 1);
+        }
+
+        private static void Sort(int[] array, int start, int end)
+        {
+            if (start >= end)
+            {
+                return;
+            }
+
+            var pivot = array[start + (end - start) / 2];
+            var index = Partition(array, start, start, pivot);
+
+            Sort(array, start, index - 1);
+            Sort(array, index, end);
+        }
+
+        private static int Partition(int[] array, int start, int end, int pivot)
+        {
+            while (start <= end)
+            {
+                while (start <= end && array[start] < pivot)
+                {
+                    start++;
+                }
+
+                while (end >= start && array[end] > pivot)
+                {
+                    end--;
+                }
+
+                if (start <= end)
+                {
+                    Swap(array, start, end);
+                    start++;
+                    end--;
+                }
+            }
+
+            return start;
+        }
+
+        private static void Swap(int[] array, int start, int end)
+        {
+            var temp = array[start];
+
+            array[start] = array[end];
+            array[end] = temp;
+        }
+    }
+}
