c6e7c1dff67e84f9b52575db645981becbe30415
Sergio Escalera <sergio.escalera.c@gmail.com>
2021-04-04 10:58:58 -0400

    345 - Reverse Vowels of a String

---
+++ src/LeetCode.UnitTests/ReverseStringFacts.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.UnitTests
+{
+    [TestClass]
+    [TestCategory("0345 - Reverse Vowels of a String")]
+    public class ReverseStringFacts
+    {
+        [TestMethod]
+        public void Example1()
+        {
+            var str = "hello";
+
+            var sol = new ReverseString();
+
+            Assert.AreEqual("holle", sol.Reverse(str));
+        }
+
+        [TestMethod]
+        public void Example2()
+        {
+            var str = "leetcode";
+
+            var sol = new ReverseString();
+
+            Assert.AreEqual("leotcede", sol.Reverse(str));
+        }
+
+        [TestMethod]
+        public void Examples()
+        {
+            var reverser = new ReverseString(c => char.IsLetter(c));
+
+            Assert.IsNull(reverser.Reverse(null));
+            Assert.AreEqual("", reverser.Reverse(""));
+            Assert.AreEqual("a", reverser.Reverse("a"));
+            Assert.AreEqual("$", reverser.Reverse("$"));
+            Assert.AreEqual("ba", reverser.Reverse("ab"));
+            Assert.AreEqual("a$", reverser.Reverse("a$"));
+            Assert.AreEqual("b$a", reverser.Reverse("a$b"));
+            Assert.AreEqual("c$ba", reverser.Reverse("a$bc"));
+            Assert.AreEqual("cb$a", reverser.Reverse("ab$c"));
+            Assert.AreEqual("cb$a?", reverser.Reverse("ab$c?"));
+            Assert.AreEqual(".c\b$a?", reverser.Reverse(".a\b$c?"));
+        }
+    }
+}
---
+++ src/LeetCode/0345-Reverse-Vowels-String.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode
+{
+    public class ReverseString
+    {
+        private readonly Func<char, bool> _shouldReverseChar;
+
+        public ReverseString(Func<char, bool> shouldReverseChar = null)
+        {
+            var vowels = new HashSet<char>(new[] { 'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U' });
+
+            _shouldReverseChar = shouldReverseChar ?? ((c) => vowels.Contains(c));
+        }
+
+        public string Reverse(string s)
+        {
+            if (string.IsNullOrEmpty(s))
+            {
+                return s;
+            }
+
+            var chars = s.ToCharArray();
+
+            var start = 0;
+            var end = chars.Length - 1;
+
+            while (start < end)
+            {
+                while (start <= end && !_shouldReverseChar(s[start]))
+                {
+                    start++;
+                }
+
+                while (start <= end && !_shouldReverseChar(s[end]))
+                {
+                    end--;
+                }
+
+                if (start < end)
+                {
+                    Swap(chars, start, end);
+                    start++;
+                    end--;
+                }
+            }
+
+            return new string(chars);
+        }
+
+        void Swap(char[] chars, int start, int end)
+        {
+            var temp = chars[start];
+
+            chars[start] = chars[end];
+            chars[end] = temp;
+        }
+    }
+}
